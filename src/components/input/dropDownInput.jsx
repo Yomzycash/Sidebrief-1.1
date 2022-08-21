8a119467257f4f8f83a58e62024db9b97c6116eb
Oluwole Sayo <sayo@sidebrief.com>
2022-08-21 16:00:46 +0100

    fixed conflict issues

--- src/components/input/dropDownInput.jsx
+++ src/components/input/dropDownInput.jsx
@@ -1,57 +1,47 @@
-import React from 'react'
-import { InputWrapper, Wrapper, Label, ErrMsg, Top, Select } from './styled';
-
+import React from "react";
+import { InputWrapper, Wrapper, Label, ErrMsg, Top, Select } from "./styled";
 
 const DropDownInput = ({
-    label,
-    labelStyle,
-    containerStyle,
-    edit,
-    error,
-    errorMessage,
-    OptionValues,
-    onSelectedChange = () =>{},
-    container,
-    placeholder,
-    secureTextEntry,
-    type,
-    ...rest
+  label,
+  labelStyle,
+  containerStyle,
+  edit,
+  error,
+  errorMessage,
+  OptionValues,
+  onSelectedChange = () => {},
+  container,
+  placeholder,
+  secureTextEntry,
+  type,
+  ...rest
 }) => {
-    const handleChange = (e) => {
-        let selectedValue = e.target.value;
-        onSelectedChange(selectedValue)
-
-    }
-        let options = OptionValues.map((data) => (
-            <option key={data.id} value={data.value}>
-                {data.value}
-            </option>))
+  const handleChange = (e) => {
+    let selectedValue = e.target.value;
+    onSelectedChange(selectedValue);
+  };
+  let options = OptionValues.map((data) => (
+    <option key={data.id} value={data.value}>
+      {data.value}
+    </option>
+  ));
   return (
     <Wrapper className={containerStyle}>
-        <Top>
-        {
-            label && <Label className={labelStyle}>{label}</Label>
-        }
+      <Top>
+        {label && <Label className={labelStyle}>{label}</Label>}
 
-        {
-            errorMessage ? 
-            (
-                <ErrMsg>{errorMessage}</ErrMsg>
-            ): null
-        }
-        </Top>
+        {errorMessage ? <ErrMsg>{errorMessage}</ErrMsg> : null}
+      </Top>
 
-        <InputWrapper>
-
-            <Select onChange={handleChange} >
-               <option>Select</option>
-               <option>Bolu</option>
-               {options}
-            </Select>
-        </InputWrapper>
-
+      <InputWrapper>
+        <Select onChange={handleChange}>
+          <option>Select</option>
+          <option>Bolu</option>
+          {options}
+        </Select>
+      </InputWrapper>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default DropDownInput
+export default DropDownInput;
