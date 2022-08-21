768ea4332a62aa8ec9e60ef3d0c998c08534ad4a
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2022-08-21 06:57:27 -0700

    made changes

--- src/components/navbar/styled.js
+++ src/components/navbar/styled.js
@@ -5,20 +5,12 @@ export const NavWrapper = styled.div`
     display: flex;  
     flex-direction: row;
     padding: 48px 80px;
-<<<<<<< Updated upstream
 
     @media screen and (max-width: 550px) {
         padding: 48px 24px;
       }
 `;
 
-=======
-    @media screen and (max-width: 550px) {
-        padding: 48px 24px;
-      }
-`;
-
->>>>>>> Stashed changes
 export const Image = styled.img`
     @media screen and (max-width: 550px) {
         width: 84px;
