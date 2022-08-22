a5d88f522b480f8ee99f6315a7d60a4bedbd2722
Jesuloba <jesulobajohn@gmail.com>
2022-08-22 23:24:53 +0100

    changed /user to /register

--- src/pages/Auth/Registration/AccountType.jsx
+++ src/pages/Auth/Registration/AccountType.jsx
@@ -7,140 +7,140 @@ import { LogoNav } from "../../../components/navbar";
 import TextsWithLink from "../../../components/texts/TextsWithLink";
 
 const AccountType = () => {
-  return (
-    <AccountTypeCont>
-      <AccountTypeHead>
-        <LogoNav />
-      </AccountTypeHead>
-      <AccountTypeBody>
-        <Top>
-          <HeadText
-            title="Get started with Sidebrief"
-            body="How would you like to use your account"
-          />
-        </Top>
-        <Middle>
-          <div>
-            <InfoOutlinedIcon sx={{ fontSize: 22 }} />
-            <p>Learn what the Sidebrief account types mean</p>
-          </div>
-        </Middle>
-        <Bottom>
-          <AccountTypeCard
-            title="As an individual"
-            body="Register your business with ease  lorem ipsum dolor imit"
-            // to="/register/user"
-            to="/user"
-          />
-          <AccountTypeCard
-            title="As a Reseller"
-            body="Register your business with ease  lorem ipsum dolor imit"
-            // to="/register/reseller"
-            to="/reseller"
-          />
-          <AccountTypeCard
-            title="As a Partner"
-            body="Register your business with ease  lorem ipsum dolor imit"
-            // to="/register/partner"
-            to="/partner"
-          />
-        </Bottom>
-      </AccountTypeBody>
-      <AccountFooter>
-        <TextsWithLink
-          text={[
-            {
-              text: "Already have an account? ",
-              link: { text: "Sign Up", to: "/account-type" },
-            },
-          ]}
-        />
-      </AccountFooter>
-    </AccountTypeCont>
-  );
+	return (
+		<AccountTypeCont>
+			<AccountTypeHead>
+				<LogoNav />
+			</AccountTypeHead>
+			<AccountTypeBody>
+				<Top>
+					<HeadText
+						title="Get started with Sidebrief"
+						body="How would you like to use your account"
+					/>
+				</Top>
+				<Middle>
+					<div>
+						<InfoOutlinedIcon sx={{ fontSize: 22 }} />
+						<p>Learn what the Sidebrief account types mean</p>
+					</div>
+				</Middle>
+				<Bottom>
+					<AccountTypeCard
+						title="As an individual"
+						body="Register your business with ease  lorem ipsum dolor imit"
+						// to="/register/register"
+						to="/register"
+					/>
+					<AccountTypeCard
+						title="As a Reseller"
+						body="Register your business with ease  lorem ipsum dolor imit"
+						// to="/register/reseller"
+						to="/reseller"
+					/>
+					<AccountTypeCard
+						title="As a Partner"
+						body="Register your business with ease  lorem ipsum dolor imit"
+						// to="/register/partner"
+						to="/partner"
+					/>
+				</Bottom>
+			</AccountTypeBody>
+			<AccountFooter>
+				<TextsWithLink
+					text={[
+						{
+							text: "Already have an account? ",
+							link: { text: "Sign Up", to: "/account-type" },
+						},
+					]}
+				/>
+			</AccountFooter>
+		</AccountTypeCont>
+	);
 };
 
 export default AccountType;
 
 const AccountTypeCont = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  gap: 3rem;
-  padding: 1.5rem 5% 4rem;
+	display: flex;
+	flex-flow: column nowrap;
+	gap: 3rem;
+	padding: 1.5rem 5% 4rem;
 `;
 
 const AccountTypeHead = styled.div`
-  width: 380px;
-  @media screen and (max-width: 550px) {
-    margin: 0 auto;
-  }
+	width: 380px;
+	@media screen and (max-width: 550px) {
+		margin: 0 auto;
+	}
 `;
 
 const AccountTypeBody = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  gap: 4rem;
-  @media screen and (max-width: 550px) {
-    align-items: flex-start;
-    margin: 0 auto;
-  }
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: center;
+	gap: 4rem;
+	@media screen and (max-width: 550px) {
+		align-items: flex-start;
+		margin: 0 auto;
+	}
 `;
 
 const Top = styled.div``;
 
 const Middle = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  div {
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    justify-content: center;
-    font-size: clamp(14px, 1.5vw, 16px);
-    background-color: var(--PrimaryBlue);
-    color: var(--SecondaryBlue);
-    gap: 0.5rem;
-    border-radius: 1rem;
-    padding: 1rem;
-    @media screen and (max-width: 550px) {
-      width: 100%;
-    }
-  }
-  @media screen and (max-width: 550px) {
-    justify-content: flex-start;
-  }
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	div {
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+		justify-content: center;
+		font-size: clamp(14px, 1.5vw, 16px);
+		background-color: var(--PrimaryBlue);
+		color: var(--SecondaryBlue);
+		gap: 0.5rem;
+		border-radius: 1rem;
+		padding: 1rem;
+		@media screen and (max-width: 550px) {
+			width: 100%;
+		}
+	}
+	@media screen and (max-width: 550px) {
+		justify-content: flex-start;
+	}
 `;
 const Bottom = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-around;
-  gap: 5%;
-  @media screen and (max-width: 550px) {
-    flex-flow: column;
-    gap: 1rem;
-    justify-content: flex-start;
-  }
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: space-around;
+	gap: 5%;
+	@media screen and (max-width: 550px) {
+		flex-flow: column;
+		gap: 1rem;
+		justify-content: flex-start;
+	}
 `;
 const AccountFooter = styled.div`
-  width: 380px;
-  @media screen and (max-width: 550px) {
-    margin: 0 auto;
-  }
-  display: none;
-  flex-flow: row wrap;
-  justify-content: flex-start;
-  align-items: center;
-  font-size: var(--SFontsize) - 4px;
-  span {
-    color: var(--SecondaryBlue);
-  }
-  @media screen and (max-width: 550px) {
-    display: flex;
-    width: 380px;
-    @media screen and (max-width: 550px) {
-      margin: 0 auto;
-    }
-  }
+	width: 380px;
+	@media screen and (max-width: 550px) {
+		margin: 0 auto;
+	}
+	display: none;
+	flex-flow: row wrap;
+	justify-content: flex-start;
+	align-items: center;
+	font-size: var(--SFontsize) - 4px;
+	span {
+		color: var(--SecondaryBlue);
+	}
+	@media screen and (max-width: 550px) {
+		display: flex;
+		width: 380px;
+		@media screen and (max-width: 550px) {
+			margin: 0 auto;
+		}
+	}
 `;
--- src/pages/Auth/Registration/accountType/accountType.jsx
+++ src/pages/Auth/Registration/accountType/accountType.jsx
@@ -8,68 +8,73 @@ import { SecondaryText } from "components/text/text";
 import { useNavigate } from "react-router-dom";
 
 const AccountType = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/login");
-  };
-  return (
-    <>
-      <Navbar />
-      <AccountTypeCont>
-        <AccountTypeBody>
-          <Top>
-            <HeadText
-              title="Get started with Sidebrief"
-              body="How would you like to use your account"
-            />
-          </Top>
-          <Middle>
-            <div>
-              <AiOutlineInfoCircle />
-              <p>Learn what the Sidebrief account types mean</p>
-            </div>
-          </Middle>
-          <Bottom>
-            <AccountTypeCard
-              title="As an individual"
-              body="Register your business with ease  lorem ipsum dolor imit"
-              // to="/register/user"
-              to="/user"
-            />
-            <AccountTypeCard
-              title="As a Reseller"
-              body="Register your business with ease  lorem ipsum dolor imit"
-              // to="/register/reseller"
-              to="/reseller"
-            />
-            <AccountTypeCard
-              title="As a Partner"
-              body="Register your business with ease  lorem ipsum dolor imit"
-              // to="/register/partner"
-              to="/partner"
-            />
-          </Bottom>
-        </AccountTypeBody>
+	const handleClick = () => {
+		navigate("/login");
+	};
+	return (
+		<>
+			<Navbar />
+			<AccountTypeCont>
+				<AccountTypeBody>
+					<Top>
+						<HeadText
+							title="Get started with Sidebrief"
+							body="How would you like to use your account"
+						/>
+					</Top>
+					<Middle>
+						<div>
+							<AiOutlineInfoCircle />
+							<p>Learn what the Sidebrief account types mean</p>
+						</div>
+					</Middle>
+					<Bottom>
+						<AccountTypeCard
+							title="As an individual"
+							body="Register your business with ease  lorem ipsum dolor imit"
+							// to="/register/register"
+							to="/register"
+						/>
+						<AccountTypeCard
+							title="As a Reseller"
+							body="Register your business with ease  lorem ipsum dolor imit"
+							// to="/register/reseller"
+							to="/reseller"
+						/>
+						<AccountTypeCard
+							title="As a Partner"
+							body="Register your business with ease  lorem ipsum dolor imit"
+							// to="/register/partner"
+							to="/partner"
+						/>
+					</Bottom>
+				</AccountTypeBody>
 
-        <AccountFooter>
-          <SecondaryText>Already have an account? </SecondaryText>
-          <SecondaryText clickColor cursor left="10px" onClick={handleClick}>
-            Sign In
-          </SecondaryText>
-        </AccountFooter>
-      </AccountTypeCont>
-    </>
-  );
+				<AccountFooter>
+					<SecondaryText>Already have an account? </SecondaryText>
+					<SecondaryText
+						clickColor
+						cursor
+						left="10px"
+						onClick={handleClick}
+					>
+						Sign In
+					</SecondaryText>
+				</AccountFooter>
+			</AccountTypeCont>
+		</>
+	);
 };
 
 export default AccountType;
 
 const AccountTypeCont = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  gap: 3rem;
-  padding: 3rem 5% 4rem;
+	display: flex;
+	flex-flow: column nowrap;
+	gap: 3rem;
+	padding: 3rem 5% 4rem;
 `;
 
 // const AccountTypeHead = styled.div`
@@ -80,67 +85,67 @@ const AccountTypeCont = styled.div`
 // `;
 
 const AccountTypeBody = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  gap: 4rem;
-  @media screen and (max-width: 550px) {
-    align-items: flex-start;
-    /* margin: 0 auto; */
-  }
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: center;
+	gap: 4rem;
+	@media screen and (max-width: 550px) {
+		align-items: flex-start;
+		/* margin: 0 auto; */
+	}
 `;
 
 const Top = styled.div`
-  @media screen and (max-width: 550px) {
-    div {
-      align-items: flex-start;
-    }
-  }
+	@media screen and (max-width: 550px) {
+		div {
+			align-items: flex-start;
+		}
+	}
 `;
 
 const Middle = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  div {
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    justify-content: center;
-    font-size: clamp(14px, 1.5vw, 16px);
-    background-color: var(--PrimaryBlue);
-    color: var(--SecondaryBlue);
-    gap: 0.5rem;
-    border-radius: 1rem;
-    padding: 1rem;
-    @media screen and (max-width: 550px) {
-      width: 100%;
-    }
-  }
-  @media screen and (max-width: 550px) {
-    justify-content: flex-start;
-    max-width: 400px;
-  }
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	div {
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+		justify-content: center;
+		font-size: clamp(14px, 1.5vw, 16px);
+		background-color: var(--PrimaryBlue);
+		color: var(--SecondaryBlue);
+		gap: 0.5rem;
+		border-radius: 1rem;
+		padding: 1rem;
+		@media screen and (max-width: 550px) {
+			width: 100%;
+		}
+	}
+	@media screen and (max-width: 550px) {
+		justify-content: flex-start;
+		max-width: 400px;
+	}
 `;
 const Bottom = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-around;
-  gap: 5%;
-  @media screen and (max-width: 550px) {
-    flex-flow: column;
-    gap: 1rem;
-    justify-content: flex-start;
-  }
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: space-around;
+	gap: 5%;
+	@media screen and (max-width: 550px) {
+		flex-flow: column;
+		gap: 1rem;
+		justify-content: flex-start;
+	}
 `;
 const AccountFooter = styled.div`
-  display: flex;
-  margin-top: 70px;
-  justify-content: center;
-  align-items: center;
-  padding: 0px auto;
-  @media screen and (max-width: 550px) {
-    padding: 0px 0px;
-    justify-content: start;
-  }
+	display: flex;
+	margin-top: 70px;
+	justify-content: center;
+	align-items: center;
+	padding: 0px auto;
+	@media screen and (max-width: 550px) {
+		padding: 0px 0px;
+		justify-content: start;
+	}
 `;
