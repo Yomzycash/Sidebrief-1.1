1c366a9b91030c4ffc4e878bc3099f21252f4aac
Jesuloba <jesulobajohn@gmail.com>
2022-08-27 16:30:35 +0100

    added deep petals

--- src/components/cards/RewardCard/index.jsx
+++ src/components/cards/RewardCard/index.jsx
@@ -10,14 +10,13 @@ import {
 	Frame,
 } from "./styles";
 import { ReactComponent as CornerPetal } from "asset/svg/cornerPetal.svg";
+import { ReactComponent as CornerPetalDeep } from "asset/svg/cornerPetalDeep.svg";
 import { ReactComponent as ArrowBlueRight } from "asset/svg/arrow-blue-right.svg";
 
 export const RewardCard = ({ image, imageAlt, title, body, action, isBig }) => {
 	return (
 		<Container isBig={isBig}>
-			<Corner>
-				<CornerPetal />
-			</Corner>
+			<Corner>{isBig ? <CornerPetalDeep /> : <CornerPetal />}</Corner>
 			<Frame>
 				{!isBig ? (
 					<ImageHolder>
