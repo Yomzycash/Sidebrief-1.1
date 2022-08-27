30e3bf1588297fad9585886715f2cea0f1eb5d8c
Jesuloba <jesulobajohn@gmail.com>
2022-08-27 13:00:59 +0100

    added style reset

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import AppRouter from "./routes/appRouter";
 import { createGlobalStyle } from "styled-components";
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from "framer-motion";
+
+// Added style reset
 const GlobalStyle = createGlobalStyle`
+*, *:before, *:after {
+	margin: 0;
+	padding: 0;
+	box-sizing: borderbox;
+}
+
 body{
   font-family: 'BR Firma', sans-serif;
 }
---
+++ src/pages/Testing/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const TestingPage = () => {
+	return (
+		<Container>
+			<h1>This is the testing Page</h1>
+		</Container>
+	);
+};
+
+export default TestingPage;
