2a348b75f2dbfc0031179c8a54d75fd2a91a91af
Jesuloba <jesulobajohn@gmail.com>
2022-08-31 18:00:10 +0100

    prettier format dateinput file

--- src/components/input/dateInput.jsx
+++ src/components/input/dateInput.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-  Wrapper,
-  Top,
-  Label,
-  ErrMsg,
-  Iconwrapper,
-  InputWrapper,
-  DateWrapper,
+	Wrapper,
+	Top,
+	Label,
+	ErrMsg,
+	Iconwrapper,
+	InputWrapper,
+	DateWrapper,
 } from "./styled";
 import { ReactComponent as CalendarIcon } from "asset/auth/Calendar.svg";
 import DatePicker from "react-datepicker";
@@ -14,79 +14,78 @@ import "react-datepicker/dist/react-datepicker.css";
 import format from "date-fns/format";
 
 export const DateInput = ({
-  containerStyle,
-  label,
-  labelStyle,
-  errorMessage,
-  leftIcon,
-  name,
-  register,
-  selectDate,
-  ...rest // rest can REST, I guess
+	containerStyle,
+	label,
+	labelStyle,
+	errorMessage,
+	leftIcon,
+	name,
+	register,
+	selectDate,
+	...rest // rest can REST, I guess
 }) => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [dateIsTouched, setDateIsTouched] = useState(false);
+	const [selectedDate, setSelectedDate] = useState(new Date());
+	const [dateIsTouched, setDateIsTouched] = useState(false);
 
-  const pickDay = (day) => {
-    setSelectedDate(day);
-    setDateIsTouched(true);
-    const selectedDate = format(day, "dd/MM/yyyy");
-    selectDate(selectedDate);
-  };
-  const [active, setActive] = useState(false);
-  const inputRef = useRef(null);
+	const pickDay = (day) => {
+		setSelectedDate(day);
+		setDateIsTouched(true);
+		const selectedDate = format(day, "dd/MM/yyyy");
+		selectDate(selectedDate);
+	};
+	const [active, setActive] = useState(false);
+	const inputRef = useRef(null);
 
-  useEffect(() => {
-    if (active) {
-      inputRef.current.focus();
-    }
-  }, [active]);
-  const handleBorder = () => {
-    setActive(!active);
-  };
+	useEffect(() => {
+		if (active) {
+			inputRef.current.focus();
+		}
+	}, [active]);
+	const handleBorder = () => {
+		setActive(!active);
+	};
 
-  return (
-    <Wrapper
-      className={containerStyle}
-      initial={{ y: 10, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      exit={{ y: 10, opacity: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Top>
-        <Label className={labelStyle}>{label ? label : "Date"}</Label>
+	return (
+		<Wrapper
+			className={containerStyle}
+			initial={{ y: 10, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			exit={{ y: 10, opacity: 0 }}
+			transition={{ duration: 0.5 }}
+		>
+			<Top>
+				<Label className={labelStyle}>{label ? label : "Date"}</Label>
 
-        {errorMessage ? <ErrMsg>{errorMessage}</ErrMsg> : null}
-      </Top>
-      <InputWrapper
-        border={
-          errorMessage
-            ? "1px solid red"
-            : active
-            ? "1px solid #00A2D4"
-            : "1px solid #ececec"
-        }
-        ref={inputRef}
-        onFocus={handleBorder}
-      >
-        <Iconwrapper>
-          <label htmlFor="date">
-            <CalendarIcon />
-          </label>
-        </Iconwrapper>
+				{errorMessage ? <ErrMsg>{errorMessage}</ErrMsg> : null}
+			</Top>
+			<InputWrapper
+				border={
+					errorMessage
+						? "1px solid red"
+						: active
+						? "1px solid #00A2D4"
+						: "1px solid #ececec"
+				}
+				ref={inputRef}
+				onFocus={handleBorder}
+			>
+				<Iconwrapper>
+					<label htmlFor="date">
+						<CalendarIcon />
+					</label>
+				</Iconwrapper>
 
-        <DateWrapper>
-          <DatePicker
-            selected={dateIsTouched ? selectedDate : ""}
-            onChange={pickDay}
-            placeholderText={"DD/MM/YY"}
-            dateFormat={"dd/MM/yyyy"}
-            closeOnScroll={true}
-            className="date"
-            id="date"
-          />
-        </DateWrapper>
-      </InputWrapper>
-    </Wrapper>
-  );
+				<DateWrapper>
+					<DatePicker
+						selected={dateIsTouched ? selectedDate : ""}
+						onChange={pickDay}
+						placeholderText={"DD/MM/YY"}
+						dateFormat={"dd/MM/yyyy"}
+						className="date"
+						id="date"
+					/>
+				</DateWrapper>
+			</InputWrapper>
+		</Wrapper>
+	);
 };
