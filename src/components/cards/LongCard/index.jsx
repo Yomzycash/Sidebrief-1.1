f1aea0e6199d0a27b578aece47ed793af2e86ff0
Jesuloba <jesulobajohn@gmail.com>
2022-09-04 14:11:34 +0100

    fixed long card with sayo

--- src/components/cards/LongCard/index.jsx
+++ src/components/cards/LongCard/index.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import {
-  Container,
-  IconWrapper,
-  MiddlePart,
-  ButtonWrapper,
-  LongButton,
-  FirstPart,
-  Title,
-  Body,
+	Container,
+	IconWrapper,
+	MiddlePart,
+	ButtonWrapper,
+	LongButton,
+	FirstPart,
+	Title,
+	Body,
 } from "./styles";
 import { ReactComponent as Case } from "asset/svg/briefCase.svg";
+import { ReactComponent as ArrowWhiteRight } from "asset/svg/arrow-white-right.svg";
 import { TextWithArrow } from "components/texts";
 
 export const LongCard = ({
-  title,
-  body,
-  notready, // used to determine if button should be disabled or not (value should be 'true' or 'false')
-  action, // function for action that should be performed on button click
+	title,
+	body,
+	notready, // used to determine if button should be disabled or not (value should be 'true' or 'false')
+	action, // function for action that should be performed on button click
 }) => {
-	const onClick = () => {
-		if (!notReady) {
-			action();
-		}
-	};
-
 	return (
-		<Container onClick={onClick} isReady={!notReady}>
+		<Container notready={notready}>
 			<FirstPart>
-				<IconWrapper>
-					<Case />
+				<IconWrapper notready={notready}>
+					<Case
+						fill={`${notready === "true" ? "#ED4E3A" : "#00a2d4"}`}
+					/>
 				</IconWrapper>
 				<MiddlePart>
-					<Title>{title}</Title>
-					<Body notReady={notReady}>{body}</Body>
+					<Title>
+						{title} {notready && <span> Coming soon</span>}
+					</Title>
+					<Body>{body}</Body>
 				</MiddlePart>
 			</FirstPart>
 			<ButtonWrapper>
 				<LongButton
-					notReady={notReady}
-					onClick={onClick}
-					disabled={notReady}
+					notready={notready}
+					onClick={action}
+					disabled={notready === "true"}
 				>
 					<TextWithArrow>
-						{!notReady ? "Get Started" : "Coming soon"}
+						{!notready ? "Get Started" : "Coming soon"}
 					</TextWithArrow>
 				</LongButton>
 			</ButtonWrapper>
