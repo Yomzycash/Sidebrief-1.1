954b53e99d11ac1fe3661050021e98ef049e8fcf
Jesuloba <jesulobajohn@gmail.com>
2022-09-13 11:52:43 +0100

    removed ref

--- src/pages/Launch/BusinessAddress/index.jsx
+++ src/pages/Launch/BusinessAddress/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import HeaderCheckout from "components/Header/HeaderCheckout";
 import { CheckoutController, CheckoutSection } from "containers";
 import { DropDownWithSearch, InputWithLabel } from "components/input";
