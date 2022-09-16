315e8072bd4750e7bba5f190510e550adf134898
femiAdeyemo <femi@sidebrief.com>
2022-09-16 08:07:45 +0100

    modal added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,10 @@
-import React from "react";
-import TagInputWithSearch from "components/input/TagInputWithSearch.jsx";
-
+import React from 'react'
+import RewardModal from "components/modal/RewardModal";
 const Home = () => {
-  const label = "Business Objectives";
-
-  const list = [
-    "Marketing",
-    "Art and Designs",
-    "Construction",
-    "Information and Technology",
-    "Marketing",
-    "Art and Designs",
-    "Construction",
-    "Information and Technology",
-    "Marketing",
-    "Art and Designs",
-    "Construction",
-    "Information and Technology",
-  ];
-
-  const getValue = (value) => {
-    console.log(value);
-  };
-
   return (
-    <div style={{ maxWidth: "70%", margin: "auto" }}>
-      <p>Welcome to sidebrief</p>
-      <TagInputWithSearch
-        label={label}
-        list={list}
-        MultiSelect
-        ExistsError="Tag has already been selected"
-        MatchError="Please select objectives from the list"
-        EmptyError="Please select at least one objective"
-        getValue={getValue}
-      />
-    </div>
-  );
-};
+    
+    <RewardModal/>
+  )
+}
 
-export default Home;
+export default Home
