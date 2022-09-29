4d40f9b452c0c1e82ea3086a2c563aca499d3105
Jesuloba <jesulobajohn@gmail.com>
2022-09-29 20:51:23 +0100

    persist state

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -3,8 +3,6 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { authApi } from "services/authService";
 import { launchApi } from "services/launchService";
 import { RewardApi } from "services/RewardService";
-import storage from "redux-persist/lib/storage";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import {
 	LaunchReducer,
 	LayoutInfoReducer,
@@ -12,37 +10,36 @@ import {
 	RewardReducer,
 	UserDataReducer,
 } from "./Slices";
-import persistStore from "redux-persist/es/persistStore";
+import { save, load } from "redux-localstorage-simple";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  stateReconciler: autoMergeLevel2,
-};
 const rootReducer = combineReducers({
-  [authApi.reducerPath]: authApi.reducer,
-  [launchApi.reducerPath]: launchApi.reducer,
-  [RewardApi.reducerPath]: RewardApi.reducer,
-  UserDataReducer: UserDataReducer,
-  LayoutInfo: LayoutInfoReducer,
-  RegisteredBusinessesInfo: RegisteredBusinessesReducers,
-  LaunchReducer: LaunchReducer,
-  RewardReducer: RewardReducer,
+	[authApi.reducerPath]: authApi.reducer,
+	[launchApi.reducerPath]: launchApi.reducer,
+	[RewardApi.reducerPath]: RewardApi.reducer,
+	UserDataReducer: UserDataReducer,
+	LayoutInfo: LayoutInfoReducer,
+	RegisteredBusinessesInfo: RegisteredBusinessesReducers,
+	LaunchReducer: LaunchReducer,
+	RewardReducer: RewardReducer,
 });
 
-const persistedReducer = persistedReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+	reducer: rootReducer,
 
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware().concat([
-  //     authApi.middleware,
-  //     launchApi.middleware,
-  //     RewardApi.middleware,
-  //   ]),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat([
+			authApi.middleware,
+			launchApi.middleware,
+			RewardApi.middleware,
+			save({
+				ignoreStates: [
+					"launchApi.middleware",
+					"authApi.middleware",
+					"RewardApi.middleware",
+				],
+			}),
+		]),
+	preloadedState: load(),
 });
 
 setupListeners(store.dispatch);
-
-export const persistor = persistStore(store);
