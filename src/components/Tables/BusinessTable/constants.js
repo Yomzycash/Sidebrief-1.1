92267c13939910e7ebdfa00ec15d4e30c9a04346
femiAdeyemo <femi@sidebrief.com>
2022-10-17 11:00:35 +0100

    drafts done

--- src/components/Tables/BusinessTable/constants.js
+++ src/components/Tables/BusinessTable/constants.js
@@ -1,87 +1,87 @@
-import React from "react";
-import { createColumnHelper } from "@tanstack/react-table";
-import { HeadText, BodyText, Checkbox } from "./styles";
-import { TypeIndicator } from "components/Indicators";
+import React from 'react'
+import { createColumnHelper } from '@tanstack/react-table'
+import { HeadText, BodyText, Checkbox } from './styles'
+import { TypeIndicator } from 'components/Indicators'
 
-const ColumnHelper = createColumnHelper();
+const ColumnHelper = createColumnHelper()
 
 export const businessTypes = [
-	{
-		name: "c-corporation",
-		code: "CCORP",
-		color: "#00D448",
-	},
-	{
-		name: "limited liablity company",
-		code: "LLC",
-		color: "#00A2D4",
-	},
-	{
-		name: "public liablity company",
-		code: "PLC",
-		color: "#D400CC",
-	},
-];
+  {
+    name: 'c-corporation',
+    code: 'CCORP',
+    color: '#00D448',
+  },
+  {
+    name: 'limited liablity company',
+    code: 'LLC',
+    color: '#00A2D4',
+  },
+  {
+    name: 'public liablity company',
+    code: 'PLC',
+    color: '#D400CC',
+  },
+]
 
 export const columns = [
-	ColumnHelper.display({
-		id: "checkbox",
-		header: ({ table }) => {
-			return (
-				<IndeterminateCheckbox
-					checked={table.getIsAllRowsSelected()}
-					indeterminate={table.getIsSomeRowsSelected()}
-					onChange={table.getToggleAllRowsSelectedHandler()}
-				/>
-			);
-		},
-		cell: ({ row }) => {
-			return (
-				<IndeterminateCheckbox
-					checked={row.getIsSelected()}
-					indeterminate={row.getIsSomeSelected()}
-					onChange={row.getToggleSelectedHandler()}
-				/>
-			);
-		},
-	}),
-	ColumnHelper.accessor("name", {
-		header: () => <HeadText>Business Name</HeadText>,
-		cell: (info) => <BodyText>{info.getValue()}</BodyText>,
-	}),
-	ColumnHelper.accessor("type", {
-		header: () => <HeadText>Type</HeadText>,
-		cell: (info) => {
-			const typeName = info.getValue();
-			const color = businessTypes.find(
-				(type) => type.name === typeName
-			).color;
-			return <TypeIndicator color={color} type={typeName} />;
-		},
-	}),
-	ColumnHelper.accessor("country", {
-		header: () => <HeadText>Country</HeadText>,
-		cell: (info) => <BodyText>{info.getValue()}</BodyText>,
-	}),
-	ColumnHelper.accessor("date", {
-		header: () => <HeadText>Date</HeadText>,
-		cell: (info) => <BodyText>{info.getValue()}</BodyText>,
-	}),
-];
+  ColumnHelper.display({
+    id: 'checkbox',
+    header: ({ table }) => {
+      return (
+        <IndeterminateCheckbox
+          checked={table.getIsAllRowsSelected()}
+          indeterminate={table.getIsSomeRowsSelected()}
+          onChange={table.getToggleAllRowsSelectedHandler()}
+        />
+      )
+    },
+    cell: ({ row }) => {
+      return (
+        <IndeterminateCheckbox
+          checked={row.getIsSelected()}
+          indeterminate={row.getIsSomeSelected()}
+          onChange={row.getToggleSelectedHandler()}
+        />
+      )
+    },
+  }),
+  ColumnHelper.accessor('name', {
+    header: () => <HeadText>Business Name</HeadText>,
+    cell: (info) => <BodyText>{info.getValue()}</BodyText>,
+  }),
+  ColumnHelper.accessor('type', {
+    header: () => <HeadText>Type</HeadText>,
+    cell: (info) => {
+      const typeName = info.getValue()
+      // const color = businessTypes.find(
+      // 	(type) => type.name === typeName
+      // ).color;
+      return <TypeIndicator color={'blue'} type={typeName} />
+    },
+  }),
+  ColumnHelper.accessor('country', {
+    header: () => <HeadText>Country</HeadText>,
+    cell: (info) => <BodyText>{info.getValue()}</BodyText>,
+  }),
+  ColumnHelper.accessor('date', {
+    header: () => <HeadText>Date</HeadText>,
+    cell: (info) => <BodyText>{info.getValue()}</BodyText>,
+  }),
+]
 
 const IndeterminateCheckbox = ({
-	indeterminate,
-	className = "",
-	checked,
-	...rest
+  indeterminate,
+  className = '',
+  checked,
+  ...rest
 }) => {
-	const ref = React.useRef(null);
+  const ref = React.useRef(null)
 
-	React.useEffect(() => {
-		if (typeof indeterminate === "boolean") {
-			ref.current.indeterminate = !checked && indeterminate;
-		}
-	}, [ref, indeterminate, checked]);
+  React.useEffect(() => {
+    if (typeof indeterminate === 'boolean') {
+      ref.current.indeterminate = !checked && indeterminate
+    }
+  }, [ref, indeterminate, checked])
 
-	return <Checkbox type="checkbox" ref={ref} checked={checked} {...rest} />;
-};
+  return <Checkbox type="checkbox" ref={ref} checked={checked} {...rest} />
+}
--- src/components/Tables/BusinessTable/index.jsx
+++ src/components/Tables/BusinessTable/index.jsx
@@ -1,66 +1,63 @@
-import React, { useState } from "react";
-import { Container, Table, Head, HeadData, Row, RowData } from "./styles";
+import React, { useState } from 'react'
+import { Container, Table, Head, HeadData, Row, RowData } from './styles'
 import {
-	useReactTable,
-	flexRender,
-	getCoreRowModel,
-} from "@tanstack/react-table";
-import { columns } from "./constants";
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+} from '@tanstack/react-table'
+import { columns } from './constants'
 
 export const BusinessTable = ({ data }) => {
-	const [rowSelection, setRowSelection] = useState({});
+  const [rowSelection, setRowSelection] = useState({})
 
-	const table = useReactTable({
-		columns,
-		data,
-		getCoreRowModel: getCoreRowModel(),
-		state: {
-			rowSelection,
-		},
-		onRowSelectionChange: setRowSelection,
-	});
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      rowSelection,
+    },
+    onRowSelectionChange: setRowSelection,
+  })
 
-	// returns SelectedRows
-	const getSelectedRows = () => {
-		return table.getSelectedRowModel().flatRows;
-	};
+  // returns SelectedRows
+  const getSelectedRows = () => {
+    return table.getSelectedRowModel().flatRows
+  }
 
-	return (
-		<Container>
-			{/* The Header is here */}
-			<Table>
-				<Head>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map((header) => (
-								<HeadData key={header.id}>
-									{header.isPlaceholder
-										? null
-										: flexRender(
-												header.column.columnDef.header,
-												header.getContext()
-										  )}
-								</HeadData>
-							))}
-						</tr>
-					))}
-				</Head>
-				<tbody>
-					{table.getRowModel().rows.map((row) => (
-						<Row key={row.id}>
-							{row.getVisibleCells().map((cell) => (
-								<RowData key={cell.id}>
-									{flexRender(
-										cell.column.columnDef.cell,
-										cell.getContext()
-									)}
-								</RowData>
-							))}
-						</Row>
-					))}
-				</tbody>
-			</Table>
-			{/* The pagination controller */}
-		</Container>
-	);
-};
+  return (
+    <Container>
+      {/* The Header is here */}
+      <Table>
+        <Head>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <HeadData key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </HeadData>
+              ))}
+            </tr>
+          ))}
+        </Head>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <Row key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <RowData key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </RowData>
+              ))}
+            </Row>
+          ))}
+        </tbody>
+      </Table>
+      {/* The pagination controller */}
+    </Container>
+  )
+}
