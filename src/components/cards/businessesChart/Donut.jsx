c70841c15d0b709db0e74ca8611ca195774ee061
Jesuloba <jesulobajohn@gmail.com>
2022-10-18 11:39:39 +0100

    completed the donut chart

--- src/components/cards/businessesChart/Donut.jsx
+++ src/components/cards/businessesChart/Donut.jsx
@@ -1,52 +1,51 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import { DonutContainer, Label } from "./styled";
 
 ChartJS.register(ArcElement);
 
-export const data = {
-	labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-	datasets: [
-		{
-			label: "# of Votes",
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: [
-				"rgba(255, 99, 132, 0.2)",
-				"rgba(54, 162, 235, 0.2)",
-				"rgba(255, 206, 86, 0.2)",
-				"rgba(75, 192, 192, 0.2)",
-				"rgba(153, 102, 255, 0.2)",
-				"rgba(255, 159, 64, 0.2)",
-			],
-			borderColor: [
-				"rgba(255, 99, 132, 1)",
-				"rgba(54, 162, 235, 1)",
-				"rgba(255, 206, 86, 1)",
-				"rgba(75, 192, 192, 1)",
-				"rgba(153, 102, 255, 1)",
-				"rgba(255, 159, 64, 1)",
-			],
-			borderWidth: 1,
-		},
-	],
-};
+export const Donut = ({ analytics }) => {
+	const label = analytics.label;
+	const noLabelAnalytics = { ...analytics };
+	delete noLabelAnalytics.label;
+	const total = Object.values(noLabelAnalytics)
+		.map((el) => el.total)
+		.reduce((a, b) => {
+			return a + b;
+		}, 0);
 
-export const Donut = (analytics) => {
-	// const label = analytics.label;
-	// const noLabelAnalytics = { ...analytics };
-	// delete noLabelAnalytics.label;
+	const data = {
+		labels: Object.values(noLabelAnalytics).map((element) => element.text),
+		datasets: [
+			{
+				label: label,
+				data: Object.values(noLabelAnalytics).map(
+					(element) => element.total
+				),
+				backgroundColor: Object.values(noLabelAnalytics).map(
+					(element) => element.color
+				),
+				borderColor: Object.values(noLabelAnalytics).map(
+					(element) => element.color
+				),
+				borderWidth: 1,
+				borderRadius: 10,
+				cutout: "85%",
+				borderJoinStyle: "bevel",
+				// borderAlign: "center",
+				// weight: 10,
+			},
+		],
+	};
 
-	// const data = {
-	// 	labels: Object.values(noLabelAnalytics).map((element) => element.text),
-	// 	datasets: [
-	// 		{
-	// 			label: label,
-	// 			data: Object.values(noLabelAnalytics).map(
-	// 				(element) => element.total
-	// 			),
-	// 		},
-	// 	],
-	// };
-
-	return <Doughnut data={data} height={128} width={128} />;
+	return (
+		<DonutContainer>
+			<Doughnut data={data} height={128} width={128} />
+			<Label>
+				<p>{total}</p>
+				<div>{analytics.label}</div>
+			</Label>
+		</DonutContainer>
+	);
 };
