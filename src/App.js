e8473d89404492a9c1304b644a5a90beec6e0040
Jesuloba <jesulobajohn@gmail.com>
2022-10-19 14:59:27 +0100

    entity page is now responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css'
-import AppRouter from './routes/appRouter'
-import { createGlobalStyle } from 'styled-components'
-import { AnimatePresence } from 'framer-motion'
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import "./App.css";
+import AppRouter from "./routes/appRouter";
+import { createGlobalStyle } from "styled-components";
+import { AnimatePresence } from "framer-motion";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import {
-  saveUserInfo,
-  setLaunchResponse,
-  setGeneratedLaunchCode,
-  setCountryISO,
-} from 'redux/Slices'
-import { store } from 'redux/Store'
-import { Country } from 'country-state-city'
+	saveUserInfo,
+	setLaunchResponse,
+	setGeneratedLaunchCode,
+	setCountryISO,
+	setCountry,
+} from "redux/Slices";
+import { store } from "redux/Store";
+import { Country } from "country-state-city";
 
 // Added style reset
 const GlobalStyle = createGlobalStyle`
@@ -19,43 +20,46 @@ body{
   font-family: 'BR Firma', sans-serif;
 
 }
-`
+`;
 
 function App() {
-  let localUserInfo = localStorage.getItem('userInfo')
-  let launchInfo = localStorage.getItem('launchInfo')
-  let countryISO = localStorage.getItem('countryISO')
+	let localUserInfo = localStorage.getItem("userInfo");
+	let launchInfo = localStorage.getItem("launchInfo");
+	let country = localStorage.getItem("country");
 
-  useEffect(() => {
-    if (localUserInfo) {
-      store.dispatch(saveUserInfo(JSON.parse(localUserInfo)))
-      //console.log(JSON.parse(localUserInfo));
-    }
-  }, [localUserInfo])
+	useEffect(() => {
+		if (localUserInfo) {
+			store.dispatch(saveUserInfo(JSON.parse(localUserInfo)));
+			//console.log(JSON.parse(localUserInfo));
+		}
+	}, [localUserInfo]);
 
-  useEffect(() => {
-    if (launchInfo) {
-      store.dispatch(setLaunchResponse(JSON.parse(launchInfo)))
-      store.dispatch(setGeneratedLaunchCode(JSON.parse(launchInfo).launchCode))
-      //console.log(JSON.parse(launchInfo));
-    }
-  }, [launchInfo])
+	useEffect(() => {
+		if (launchInfo) {
+			store.dispatch(setLaunchResponse(JSON.parse(launchInfo)));
+			store.dispatch(
+				setGeneratedLaunchCode(JSON.parse(launchInfo).launchCode)
+			);
+			//console.log(JSON.parse(launchInfo));
+		}
+	}, [launchInfo]);
 
-  useEffect(() => {
-    if (countryISO) {
-      store.dispatch(setCountryISO(countryISO))
-      //console.log(countryISO);
-    }
-  }, [countryISO])
+	useEffect(() => {
+		if (country) {
+			store.dispatch(setCountryISO(JSON.parse(country).ISO));
+			store.dispatch(setCountry(JSON.parse(country).name));
+			// console.log(country);
+		}
+	}, [country]);
 
-  return (
-    <>
-      <AnimatePresence exitBeforeEnter>
-        <GlobalStyle />
-        <AppRouter />
-      </AnimatePresence>
-    </>
-  )
+	return (
+		<>
+			<AnimatePresence exitBeforeEnter>
+				<GlobalStyle />
+				<AppRouter />
+			</AnimatePresence>
+		</>
+	);
 }
 
-export default App
+export default App;
