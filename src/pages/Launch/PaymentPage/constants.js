da9acd33f9f8d60f23b320949f47065e2fc8b93f
Jesuloba <jesulobajohn@gmail.com>
2022-10-20 13:25:18 +0100

    payment page responsive

--- src/pages/Launch/PaymentPage/constants.js
+++ src/pages/Launch/PaymentPage/constants.js
@@ -5,24 +5,24 @@ import InterSwitchImg from "asset/payment/interswitch.png";
 import Paystack from "asset/payment/Paystack.png";
 
 export const paymentProviders = [
-  {
-    name: "Paystack",
-    image: Paystack,
-  },
-  {
-    name: "remita",
-    image: RemitaImg,
-  },
-  {
-    name: "interswitch",
-    image: InterSwitchImg,
-  },
-  {
-    name: "paypal",
-    image: PaypalImg,
-  },
-  {
-    name: "flutterwave",
-    image: FlutterwaveImg,
-  },
+	{
+		name: "Paystack",
+		image: Paystack,
+	},
+	// {
+	// 	name: "remita",
+	// 	image: RemitaImg,
+	// },
+	// {
+	// 	name: "interswitch",
+	// 	image: InterSwitchImg,
+	// },
+	// {
+	// 	name: "paypal",
+	// 	image: PaypalImg,
+	// },
+	// {
+	// 	name: "flutterwave",
+	// 	image: FlutterwaveImg,
+	// },
 ];
