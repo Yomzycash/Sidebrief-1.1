4c33312dc48b19db1b49e4f44f9a396fa01e7ebc
Jesuloba <jesulobajohn@gmail.com>
2022-10-22 04:53:48 +0100

    fix: status card and chart

--- src/components/cards/StatusCard/index.jsx
+++ src/components/cards/StatusCard/index.jsx
@@ -1,39 +1,39 @@
 import React, { useState } from "react";
 import {
-  Container,
-  TextContainer,
-  ThreeDotContainer,
-  Name,
-  Top,
-  Description,
+	Container,
+	TextContainer,
+	ThreeDotContainer,
+	Name,
+	Top,
+	Description,
 } from "./styles";
 import { ReactComponent as ThreeDot } from "asset/svg/threeDot.svg";
 import { StatusIndicator } from "components/Indicators";
 
 export const StatusCard = ({
-  name, // string
-  status, // "completed" | "awaiting" | "progress" | "declined"
-  ShortDescription,
+	name, // string
+	status,
+	ShortDescription,
 }) => {
-  const [hover, setHover] = useState(false);
+	const [hover, setHover] = useState(false);
 
-  return (
-    <Container
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      hover={hover}
-    >
-      <TextContainer>
-        <Top>
-          <Name>{name}</Name>
-          <StatusIndicator status={status} />
-        </Top>
-        <ThreeDotContainer>
-          {/* Doesn't exactly have a function yet */}
-          <ThreeDot />
-        </ThreeDotContainer>
-      </TextContainer>
-      <Description hover={hover}>{ShortDescription}</Description>
-    </Container>
-  );
+	return (
+		<Container
+			onMouseEnter={() => setHover(true)}
+			onMouseLeave={() => setHover(false)}
+			hover={hover}
+		>
+			<TextContainer>
+				<Top>
+					<Name>{name}</Name>
+					<StatusIndicator status={status} />
+				</Top>
+				<ThreeDotContainer>
+					{/* Doesn't exactly have a function yet */}
+					<ThreeDot />
+				</ThreeDotContainer>
+			</TextContainer>
+			<Description hover={hover}>{ShortDescription}</Description>
+		</Container>
+	);
 };
