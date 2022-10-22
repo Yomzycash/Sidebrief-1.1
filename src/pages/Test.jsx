91756f198bf57a51b9b79d694821a4b8e9c53ee9
femiAdeyemo <femi@sidebrief.com>
2022-10-22 20:50:48 +0100

    staff info card

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,48 +1,50 @@
-import Pagination from "components/Pagination";
-import React from "react";
-import { BusinessesChartCard } from "components/cards";
-import { BusinessHomeTable } from "components/Staff/Tables";
+import Pagination from 'components/Pagination'
+import React from 'react'
+import { BusinessesChartCard } from 'components/cards'
+import { BusinessHomeTable } from 'components/Staff/Tables'
+import StaffBusinessInfoCard from 'components/cards/StaffBsinessInfoCard/Index'
 
 const Test = () => {
   const analytics = {
-    title: "User analytics",
+    title: 'User analytics',
     status1: {
-      text: "Total Users",
+      text: 'Total Users',
       total: 825,
-      color: "rgba(255, 255, 255, 0.4)",
+      color: 'rgba(255, 255, 255, 0.4)',
     },
     status2: {
-      text: "Registrations",
+      text: 'Registrations',
       total: 450,
-      color: "#ffffff",
+      color: '#ffffff',
     },
-  };
+  }
 
   const tableMockData = [
     {
-      name: "Slideshow Africa",
-      country: "Nigeria",
-      date: "12/12/2022",
+      name: 'Slideshow Africa',
+      country: 'Nigeria',
+      date: '12/12/2022',
     },
     {
-      name: "Ayomide Africa",
-      country: "Nigeria",
-      date: "12/12/2022",
+      name: 'Ayomide Africa',
+      country: 'Nigeria',
+      date: '12/12/2022',
     },
     {
-      name: "Image Deity Industries",
-      country: "Kenya",
-      date: "12/12/2022",
+      name: 'Image Deity Industries',
+      country: 'Kenya',
+      date: '12/12/2022',
     },
-  ];
+  ]
 
   return (
     <>
       {/* <Pagination /> */}
-      <BusinessesChartCard staff analytics={analytics} />
-      <BusinessHomeTable data={tableMockData} />
+      {/* <BusinessesChartCard staff analytics={analytics} />
+      <BusinessHomeTable data={tableMockData} /> */}
+      <StaffBusinessInfoCard />
     </>
-  );
-};
+  )
+}
 
-export default Test;
+export default Test
