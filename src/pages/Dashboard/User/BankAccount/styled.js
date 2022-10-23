f37fcc1b3ae5406348cfcab5c746458749a09045
femiAdeyemo <femi@sidebrief.com>
2022-10-24 00:28:00 +0100

    coming soon

--- src/pages/Dashboard/User/BankAccount/styled.js
+++ src/pages/Dashboard/User/BankAccount/styled.js
@@ -1,27 +1,30 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
   flex-flow: column;
   flex: 1;
-`;
-export const Top = styled.div``;
+`
+export const Top = styled.div``
 export const Body = styled.div`
   display: flex;
   flex-flow: column;
+  justify-content: center;
+  align-items: center;
   padding-top: 150px;
-`;
+`
 export const Main = styled.div`
   display: flex;
   flex-flow: column;
   gap: 20px;
   justify-content: center;
   align-items: center;
-  margin: auto 250px;
-`;
+  margin: auto 0;
+  max-width: 628px;
+`
 export const Image = styled.img`
   margin-bottom: 24px;
-`;
+`
 export const BoldText = styled.h3`
   color: #151717;
   font-size: clamp(20px, 1.5vw, 28px);
@@ -31,10 +34,10 @@ export const BoldText = styled.h3`
   margin-bottom: ${(props) => props.bottom};
   margin-right: ${(props) => props.right};
   margin-left: ${(props) => props.left};
-`;
+`
 
 export const ParagraphText = styled.p`
-  color: ${(props) => (props.clickColor ? "#00a2d4" : "#4E5152")};
+  color: ${(props) => (props.clickColor ? '#00a2d4' : '#4E5152')};
   font-size: clamp(14px, 1.5vw, 16px);
   font-weight: 400;
   text-align: ${(props) => props.align};
@@ -43,13 +46,13 @@ export const ParagraphText = styled.p`
   margin-right: ${(props) => props.right};
   margin-left: ${(props) => props.left};
   width: ${(props) => props.width};
-  cursor: ${(props) => props.cursor && "pointer"};
-`;
+  cursor: ${(props) => props.cursor && 'pointer'};
+`
 
 export const ComingBtn = styled.button`
   border: 1px solid #00a2d4;
   padding: 16px 7px;
-  width: 500px;
+  width: 100%;
   color: #00a2d4;
   background: none;
   border-radius: 8px;
@@ -60,4 +63,4 @@ export const ComingBtn = styled.button`
     background-color: #00a2d4;
     color: white;
   }
-`;
+`
--- src/pages/Dashboard/User/Home/IntellectualAssets/index.jsx
+++ src/pages/Dashboard/User/Home/IntellectualAssets/index.jsx
@@ -1,17 +1,39 @@
-import React from "react";
-import { Container, Body, Main } from "./styled";
-import TabNavBar from "components/TabNavBar/TabNavBar";
-import CommingSoon from "components/ComingSoon";
+import React from 'react'
+import TabNavBar from 'components/TabNavBar/TabNavBar'
+import { useNavigate } from 'react-router-dom'
+import image from '../../../../../asset/images/coming.png'
+import {
+  Body,
+  BoldText,
+  ComingBtn,
+  Container,
+  Image,
+  Main,
+  ParagraphText,
+} from './styled'
 
 const InetellectualAssets = () => {
+  const navigate = useNavigate()
   return (
     <Container>
       <TabNavBar />
       <Body>
-        <CommingSoon />
+        <Main>
+          <Image src={image} alt="" />
+          <BoldText>Coming Soon...</BoldText>
+          <ParagraphText align="center">
+            Uh oh, our apologies. The page you’re looking for is unavailable at
+            the moment. However once it’s live, you’ll be the first to know.
+          </ParagraphText>
+          <ComingBtn
+            onClick={() => navigate('/dashboard/business-registration')}
+          >
+            Back to Dashboard
+          </ComingBtn>
+        </Main>
       </Body>
     </Container>
-  );
-};
+  )
+}
 
-export default InetellectualAssets;
+export default InetellectualAssets
