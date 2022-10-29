425066b0e9c47f0f7a84b475b954d416aeac758f
Jesuloba <jesulobajohn@gmail.com>
2022-10-29 17:03:13 +0100

    mobile navigate to detail done

--- src/components/cards/BusinessAddressCard/Index.jsx
+++ src/components/cards/BusinessAddressCard/Index.jsx
@@ -1,66 +1,80 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { navigateToDetailPage } from "components/Tables/BusinessTable/constants";
+import { useNavigate } from "react-router-dom";
 
-const BusinessesCard = ({ name, type }) => {
-  return (
-    <Wrapper>
-      <InnerContainer>
-        <NameWrapper>{name}</NameWrapper>
-        <TypeWrapper>
-          <TypeContent>{type}</TypeContent>
-        </TypeWrapper>
-      </InnerContainer>
-    </Wrapper>
-  )
-}
+const BusinessesCard = ({ name, type, code, countryISO }) => {
+	const navigate = useNavigate();
+	const launchInfo = {
+		launchCode: code,
+		registrationCountry: countryISO,
+		registrationType: type,
+	};
 
-export default BusinessesCard
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
-  width: 100%;
-  padding: 24px 16px;
-  background: #ffffff;
-  border: 1px solid #edf1f7;
-  box-shadow: 0px 10px 10px -5px rgba(149, 150, 151, 0.04);
-  border-radius: 16px;
-`
+	return (
+		<Wrapper onClick={() => navigateToDetailPage(navigate, launchInfo)}>
+			<InnerContainer>
+				<Name>{name}</Name>
+				<TypeWrapper>
+					<TypeContent>{type}</TypeContent>
+				</TypeWrapper>
+			</InnerContainer>
+		</Wrapper>
+	);
+};
+
+export default BusinessesCard;
+
+const Wrapper = styled.button`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: flex-start;
+	width: 100%;
+	padding: 24px 16px;
+	background: #ffffff;
+	border: 1px solid #edf1f7;
+	box-shadow: 0px 10px 10px -5px rgba(149, 150, 151, 0.04);
+	border-radius: 16px;
+	cursor: pointer;
+`;
 const InnerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0px;
-  gap: 24px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0px;
+	gap: 24px;
 
-  width: 100%;
-  min-height: 42px;
-`
-const NameWrapper = styled.div`
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 21px;
-  letter-spacing: -0.02em;
-  color: #242627;
-`
+	width: 100%;
+	min-height: 42px;
+`;
+const Name = styled.p`
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 21px;
+	letter-spacing: -0.02em;
+	color: #242627;
+	font-family: "BR Firma";
+	text-transform: capitalize;
+`;
 const TypeWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 4px 16px;
-  gap: 10px;
-  background: rgba(0, 212, 72, 0.05);
-  border-radius: 12px;
-`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 4px 16px;
+	gap: 10px;
+	background: rgba(0, 212, 72, 0.05);
+	border-radius: 12px;
+`;
 const TypeContent = styled.div`
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 18px;
-  letter-spacing: 0.01em;
+	font-family: "BR Firma";
+	font-style: normal;
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 18px;
+	letter-spacing: 0.01em;
 
-  color: #00a2d4;
-`
+	color: #00a2d4;
+`;
--- src/pages/Dashboard/User/Business/DraftApplications/index.jsx
+++ src/pages/Dashboard/User/Business/DraftApplications/index.jsx
@@ -1,102 +1,108 @@
-import { BusinessTable } from 'components/Tables'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import { BusinessTable } from "components/Tables";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import {
-  useGetAllCountriesQuery,
-  useGetUserDraftQuery,
-} from 'services/launchService'
-import { Body, Container, Loading } from './styled'
-import { format, compareDesc } from 'date-fns'
-import { Puff } from 'react-loading-icons'
-import { useMediaQuery } from '@mui/material'
-import BusinessesCard from 'components/cards/BusinessAddressCard/Index'
-import styled from 'styled-components'
+	useGetAllCountriesQuery,
+	useGetUserDraftQuery,
+} from "services/launchService";
+import { Body, Container, Loading } from "./styled";
+import { format, compareDesc } from "date-fns";
+import { Puff } from "react-loading-icons";
+import { useMediaQuery } from "@mui/material";
+import BusinessesCard from "components/cards/BusinessAddressCard";
+import styled from "styled-components";
 
 const DraftApplications = () => {
-  const { data, error, isLoading, isSuccess } = useGetUserDraftQuery()
+	const { data, error, isLoading, isSuccess } = useGetUserDraftQuery();
 
-  const countries = useGetAllCountriesQuery()
+	const countries = useGetAllCountriesQuery();
 
-  const [dataArr, setDataArr] = useState([])
-  useEffect(() => {
-    if (isSuccess && countries.isSuccess) {
-      const response = [...data]
-      response.sort((launch1, launch2) => {
-        return compareDesc(
-          new Date(launch1.updatedAt),
-          new Date(launch2.updatedAt),
-        )
-      })
-      setDataArr(response)
-    }
-  }, [data, isSuccess, countries.isSuccess])
-  const matches = useMediaQuery('(max-width:700px)')
+	const [dataArr, setDataArr] = useState([]);
+	useEffect(() => {
+		if (isSuccess && countries.isSuccess) {
+			const response = [...data];
+			response.sort((launch1, launch2) => {
+				return compareDesc(
+					new Date(launch1.updatedAt),
+					new Date(launch2.updatedAt)
+				);
+			});
+			setDataArr(response);
+		}
+	}, [data, isSuccess, countries.isSuccess]);
+	const matches = useMediaQuery("(max-width:700px)");
 
-  return (
-    <Container>
-      <Body>
-        {isLoading ||
-          (countries.isLoading && (
-            <Loading>
-              <Puff stroke="#00A2D4" />
-            </Loading>
-          ))}
-        {!matches && dataArr.length > 0 && (
-          <BusinessTable
-            data={dataArr.map((element) => {
-              return {
-                name: element.businessNames
-                  ? element.businessNames.businessName1
-                  : 'No name ',
-                type: element?.registrationType,
-                country: countries?.data?.find(
-                  (country) =>
-                    country.countryISO === element.registrationCountry,
-                ).countryName,
-                date: format(new Date(element.updatedAt), 'dd/MM/yyyy'),
-                code: element.launchCode,
-                countryISO: element.registrationCountry,
-              }
-            })}
-          />
-        )}
-        {matches && dataArr.length > 0 && (
-          <MobileContainer>
-            {dataArr.map((element) => {
-              return (
-                <BusinessesCard
-                  name={
-                    element.businessNames
-                      ? element.businessNames.businessName1
-                      : 'No name '
-                  }
-                  type={element?.registrationType}
-                />
-              )
-            })}
-          </MobileContainer>
-        )}
-        {error?.status === 'FETCH_ERROR' ||
-        countries?.isLoading === 'FETCH_ERROR' ? (
-          <p>Please check your internet connection</p>
-        ) : (
-          ''
-        )}
-        {/* {console.log(countries.isError)} */}
-      </Body>
-    </Container>
-  )
-}
+	return (
+		<Container>
+			<Body>
+				{isLoading ||
+					(countries.isLoading && (
+						<Loading>
+							<Puff stroke="#00A2D4" />
+						</Loading>
+					))}
+				{!matches && dataArr.length > 0 ? (
+					<BusinessTable
+						data={dataArr.map((element) => {
+							return {
+								name: element.businessNames
+									? element.businessNames.businessName1
+									: "No name ",
+								type: element?.registrationType,
+								country: countries?.data?.find(
+									(country) =>
+										country.countryISO ===
+										element.registrationCountry
+								).countryName,
+								date: format(
+									new Date(element.updatedAt),
+									"dd/MM/yyyy"
+								),
+								code: element.launchCode,
+								countryISO: element.registrationCountry,
+							};
+						})}
+					/>
+				) : (
+					<MobileContainer>
+						{dataArr.map((element) => {
+							return (
+								<BusinessesCard
+									name={
+										element.businessNames
+											? element.businessNames
+													.businessName1
+											: "No name "
+									}
+									type={element?.registrationType}
+									code={element?.launchCode}
+									countryISO={element?.registrationCountry}
+								/>
+							);
+						})}
+					</MobileContainer>
+				)}
+				{error?.status === "FETCH_ERROR" ||
+				countries?.isLoading === "FETCH_ERROR" ? (
+					<p>Please check your internet connection</p>
+				) : (
+					""
+				)}
+				{/* {console.log(countries.isError)} */}
+			</Body>
+		</Container>
+	);
+};
 
-export default DraftApplications
+export default DraftApplications;
 
 const MobileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: inherit;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	max-width: inherit;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
 
-  gap: 8px;
-`
+	gap: 8px;
+`;
