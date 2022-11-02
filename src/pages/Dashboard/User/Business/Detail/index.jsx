0d16c3e5caaed85e9ea363238f609b744608dc5b
Jesuloba <jesulobajohn@gmail.com>
2022-11-02 14:33:34 +0100

    added date to stepbar

--- src/pages/Dashboard/User/Business/Detail/index.jsx
+++ src/pages/Dashboard/User/Business/Detail/index.jsx
@@ -1,111 +1,117 @@
-import { DetailContainer, DetailWrapper, Loader } from './styles'
-import { StepBar } from 'components/Indicators'
-import StaffBusinessInfoCard from 'components/cards/StaffBusinessInfoCard'
-import { useSelector } from 'react-redux'
-import { useViewLaunchRequestQuery } from 'services/launchService'
-import { Puff } from 'react-loading-icons'
-import { Dialog, DialogContent, useMediaQuery } from '@mui/material'
-import styled from 'styled-components'
-import { useState } from 'react'
+import { DetailContainer, DetailWrapper, Loader } from "./styles";
+import { StepBar } from "components/Indicators";
+import StaffBusinessInfoCard from "components/cards/StaffBusinessInfoCard";
+import { useSelector } from "react-redux";
+import { useViewLaunchRequestQuery } from "services/launchService";
+import { Puff } from "react-loading-icons";
+import { Dialog, DialogContent, useMediaQuery } from "@mui/material";
+import styled from "styled-components";
+import { useState } from "react";
 
 const BusinessDetail = () => {
-  const [open, setOpen] = useState(false)
-  const launchResponse = useSelector(
-    (store) => store.LaunchReducer.launchResponse,
-  )
-  const { data, isLoading } = useViewLaunchRequestQuery(launchResponse)
-  const matches = useMediaQuery('(max-width:700px)')
-  const handleClickOpen = () => {
-    setOpen(true)
-  }
+	const [open, setOpen] = useState(false);
+	const launchResponse = useSelector(
+		(store) => store.LaunchReducer.launchResponse
+	);
+	const { data, isLoading } = useViewLaunchRequestQuery(launchResponse);
+	const matches = useMediaQuery("(max-width:700px)");
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
 
-  const handleClose = () => {
-    setOpen(false)
-  }
-  const StepbarStyle = {
-    padding: 0,
-    backgroundColor: 'white',
-    width: 'max-width',
-    borderRadius: '16px',
-    boxShadow:
-      '0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)',
-  }
-  return (
-    <>
-      {isLoading ? (
-        <Loader>
-          <Puff stroke="#00A2D4" fill="white" />
-        </Loader>
-      ) : (
-        <DetailWrapper>
-          {matches && (
-            <StatusWrapper>
-              <Status> Status</Status>
-              <LowerContainer>
-                <StatusContent>In Progress</StatusContent>
-                <TimeLine onClick={handleClickOpen}>View timeline</TimeLine>
-                <Dialog onClose={handleClose} open={open}>
-                  <DialogContent style={StepbarStyle}>
-                    <StepBar mobile handleClose={handleClose} />
-                  </DialogContent>
-                </Dialog>
-              </LowerContainer>
-            </StatusWrapper>
-          )}
-          <DetailContainer>
-            <StaffBusinessInfoCard
-              businessNames={data.businessNames}
-              businessObjectives={data.businessObjects}
-              address={data.businessAddress}
-              type={data.registrationType}
-            />
-            <StepBar />
-          </DetailContainer>
-        </DetailWrapper>
-      )}
-    </>
-  )
-}
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const StepbarStyle = {
+		padding: 0,
+		backgroundColor: "white",
+		width: "max-width",
+		borderRadius: "16px",
+		boxShadow:
+			"0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)",
+	};
+	return (
+		<>
+			{isLoading ? (
+				<Loader>
+					<Puff stroke="#00A2D4" fill="white" />
+				</Loader>
+			) : (
+				<DetailWrapper>
+					{matches && (
+						<StatusWrapper>
+							<Status> Status</Status>
+							<LowerContainer>
+								<StatusContent>In Progress</StatusContent>
+								<TimeLine onClick={handleClickOpen}>
+									View timeline
+								</TimeLine>
+								<Dialog onClose={handleClose} open={open}>
+									<DialogContent style={StepbarStyle}>
+										<StepBar
+											applied={data.createdAt}
+											mobile
+											handleClose={handleClose}
+										/>
+									</DialogContent>
+								</Dialog>
+							</LowerContainer>
+						</StatusWrapper>
+					)}
+					<DetailContainer>
+						<StaffBusinessInfoCard
+							businessNames={data.businessNames}
+							businessObjectives={data.businessObjects}
+							address={data.businessAddress}
+							type={data.registrationType}
+						/>
+						<StepBar applied={data.createdAt} />
+					</DetailContainer>
+				</DetailWrapper>
+			)}
+		</>
+	);
+};
 
-export default BusinessDetail
+export default BusinessDetail;
 
 const StatusWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-  gap: 8px;
-  width: 100%;
-`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 0px;
+	gap: 8px;
+	width: 100%;
+`;
 const Status = styled.h3`
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 21px;
-  color: #4e5152;
-`
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 21px;
+	color: #4e5152;
+`;
 const LowerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 
-  justify-content: space-between;
-  padding-block: 4px;
-  width: 100%;
-`
+	justify-content: space-between;
+	padding-block: 4px;
+	width: 100%;
+`;
 const StatusContent = styled.div`
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 18px;
-  letter-spacing: 0.01em;
-  color: #ffbf29;
-  background: rgba(255, 191, 41, 0.05);
-  border-radius: 12px;
-  padding: 4px 16px;
-`
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 18px;
+	letter-spacing: 0.01em;
+	color: #ffbf29;
+	background: rgba(255, 191, 41, 0.05);
+	border-radius: 12px;
+	padding: 4px 16px;
+`;
 const TimeLine = styled.div`
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 21px;
-  cursor: pointer;
-  color: #00a2d4;
-`
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 21px;
+	cursor: pointer;
+	color: #00a2d4;
+`;
