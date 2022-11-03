aa00d7d5a5dc6f9c06f5b84944f2df3972a870b3
Jesuloba <jesulobajohn@gmail.com>
2022-11-03 06:00:19 +0100

    started using env variables in places that might need them

--- src/services/launchService.js
+++ src/services/launchService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const launchApi = createApi({
 	reducerPath: "launchApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://api.sidebrief.com/",
+		baseUrl: `${process.env.REACT_APP_DEV_BASE_URL}`,
 		prepareHeaders: (headers, { getState }) => {
 			const token = getState().UserDataReducer.userInfo.token;
 			// const token =
