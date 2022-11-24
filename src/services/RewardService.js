e0ec36ad9683f905a7cfff65a1d6c1fbe7922c89
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2022-11-24 22:52:13 +0100

    delete dialog

--- src/services/RewardService.js
+++ src/services/RewardService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const RewardApi = createApi({
   reducerPath: "RewardApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.sidebrief.com/",
+    baseUrl: process.env.REACT_APP_DEV_BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().UserDataReducer.userInfo.token;
       console.log(token);
