990c9620a0fa0b7b06ba3baf620fc95c4fde5160
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2022-12-02 13:09:24 +0100

    changes

--- src/components/profileDetails/index.jsx
+++ src/components/profileDetails/index.jsx
@@ -2,6 +2,7 @@ import { InputWithLabel } from "components/input";
 import React from "react";
 import {
   AlternateEmailDetails,
+  ButtonDetils,
   EmailDetails,
   EmailWrapper,
   InputWrapper,
@@ -14,52 +15,66 @@ import {
   TextContainer,
   TextParagraph,
   TextTitle,
+  UpperDetails,
+  UserDetails,
 } from "./style";
 import { ReactComponent as User } from "../../asset/svg/profileUser.svg";
 const ProfileDetails = () => {
   return (
-    <ProfileContainer>
-      <PhotoDetails>
-        <TextContainer>
-          <TextTitle>Your Photo</TextTitle>
-          <TextParagraph>This will be displayed on your profile</TextParagraph>
-        </TextContainer>
+    <>
+      {/* <UpperDetails>
+        <UserDetails>
+          <PhotoImage>
+            <User />
+          </PhotoImage>
+        </UserDetails>
+        <ButtonDetils></ButtonDetils>
+      </UpperDetails> */}
+      <ProfileContainer>
+        <PhotoDetails>
+          <TextContainer>
+            <TextTitle>Your Photo</TextTitle>
+            <TextParagraph>
+              This will be displayed on your profile
+            </TextParagraph>
+          </TextContainer>
 
-        <PhotoImage>
-          <User />
-        </PhotoImage>
-      </PhotoDetails>
+          <PhotoImage>
+            <User />
+          </PhotoImage>
+        </PhotoDetails>
 
-      <EmailDetails>
-        <TextContainer>
-          <TextTitle>Work email address</TextTitle>
-          <TextParagraph>This cannot be changed by you</TextParagraph>
-        </TextContainer>
-        <EmailWrapper>
-          <p>akinyemibamidele2@gmail.com</p>
-        </EmailWrapper>
-      </EmailDetails>
-      <AlternateEmailDetails>
-        <TextContainer>
-          <TextTitle>Alternative email address</TextTitle>
-          <TextParagraph>
-            This will be used to contact you as an alternate means
-          </TextParagraph>
-        </TextContainer>
-        <InputWrapper>
-          <InputWithLabel placeholder={"akinyemibamidele2@gmail.com"} />
-        </InputWrapper>
-      </AlternateEmailDetails>
+        <EmailDetails>
+          <TextContainer>
+            <TextTitle>Work email address</TextTitle>
+            <TextParagraph>This cannot be changed by you</TextParagraph>
+          </TextContainer>
+          <EmailWrapper>
+            <p>akinyemibamidele2@gmail.com</p>
+          </EmailWrapper>
+        </EmailDetails>
+        <AlternateEmailDetails>
+          <TextContainer>
+            <TextTitle>Alternative email address</TextTitle>
+            <TextParagraph>
+              This will be used to contact you as an alternate means
+            </TextParagraph>
+          </TextContainer>
+          <InputWrapper>
+            <InputWithLabel placeholder={"akinyemibamidele2@gmail.com"} />
+          </InputWrapper>
+        </AlternateEmailDetails>
 
-      <PasswordDetails>
-        <TextContainer>
-          <TextTitle>Password</TextTitle>
-        </TextContainer>
-        <InputWrapper>
-          <InputWithLabel password placeholder={"123456"} />
-        </InputWrapper>
-      </PasswordDetails>
-    </ProfileContainer>
+        <PasswordDetails>
+          <TextContainer>
+            <TextTitle>Password</TextTitle>
+          </TextContainer>
+          <InputWrapper>
+            <InputWithLabel password placeholder={"123456"} />
+          </InputWrapper>
+        </PasswordDetails>
+      </ProfileContainer>
+    </>
   );
 };
 
