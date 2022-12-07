f5a1312c6f7bec5dd2fccc93cf812da49a5c641d
femiAdeyemo <femi@sidebrief.com>
2022-12-07 16:04:38 +0100

    entities Api

--- src/pages/Dashboard/staffDashboard/Businesses/StaffEntities/StaffEntities.jsx
+++ src/pages/Dashboard/staffDashboard/Businesses/StaffEntities/StaffEntities.jsx
@@ -1,24 +1,37 @@
-import Navbar from "components/navbar";
-import React from "react";
-import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
-import styled from "styled-components";
+import Navbar from 'components/navbar'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+import styled from 'styled-components'
 
-import StaffSidebar from "components/sidebar/StaffSidebar";
-import StaffHeader from "components/Header/StaffHeader";
-import { CountryCardDetails, EntityCardDetails } from "utils/config";
-import StaffEntityCard from "components/cards/StaffEntityCard";
+import StaffSidebar from 'components/sidebar/StaffSidebar'
+import StaffHeader from 'components/Header/StaffHeader'
+import { CountryCardDetails, EntityCardDetails } from 'utils/config'
+import StaffEntityCard from 'components/cards/StaffEntityCard'
+import { useGetAllTheEntitiesQuery } from 'services/launchService'
+import { Puff } from "react-loading-icons";
+
 
 const StaffEntities = () => {
-  const layoutInfo = useSelector((store) => store.LayoutInfo);
-  const { sidebarWidth } = layoutInfo;
+  const [entities, setEntities] = useState([])
+
+  const layoutInfo = useSelector((store) => store.LayoutInfo)
+  const { sidebarWidth } = layoutInfo
+  const { data, isLoading, isSuccess, isError } = useGetAllTheEntitiesQuery()
+  console.log(data)
+
+  useEffect(() => {
+    if (data) {
+      setEntities(data)
+    }
+  }, [data])
 
   return (
     <Dashboard>
       <Navbar
         dashboard
-        imgStyles={{ maxWidth: "100px" }}
-        style={{ padding: "12px 24px" }}
+        imgStyles={{ maxWidth: '100px' }}
+        style={{ padding: '12px 24px' }}
         hideSearch
       />
       <Body>
@@ -28,22 +41,28 @@ const StaffEntities = () => {
         <BodyRight SidebarWidth={sidebarWidth}>
           <StaffHeader
             title="Entities"
-            shown="12"
-            total="12"
+            shown={entities.length }
+            total={entities.length}
             Description="Add Entity"
             placeholder="Search an entity"
           />
+          {isLoading && (
+            <Loading height="300px">
+              <Puff stroke="#00A2D4" fill="white" />
+            </Loading>
+          )}
           <CardContainer>
             <CardWrapper>
-              {EntityCardDetails.map((entity, index) => (
+              {entities.map((entity, index) => (
                 <StaffEntityCard
                   key={entity.id}
-                  entityName={entity.entityName}
-                  shareholderType={entity.entityName}
-                  entityTimeline={entity.entityTimeline}
-                  entityType={entity.entityType}
-                  countryCode={entity.countryCode}
-                  entityPackage={entity.entityPackage}
+                  entityName={entity?.entityName}
+                  entityCode={entity?.entityShortName}                  
+                  shareholderType={entity?.entityDescription}
+                  entityTimeline={entity?.entityTimeline}
+                  entityType={entity?.entityType}
+                  countryCode={entity?.entityCountry}
+                  entityPackage={entity?.entityRequirements}
                 />
               ))}
             </CardWrapper>
@@ -51,28 +70,28 @@ const StaffEntities = () => {
         </BodyRight>
       </Body>
     </Dashboard>
-  );
-};
+  )
+}
 
-export default StaffEntities;
+export default StaffEntities
 const Dashboard = styled.div`
   display: flex;
   flex-flow: column;
   width: 100%;
   flex: 1;
-`;
+`
 const Body = styled.div`
   display: flex;
   flex-flow: row nowrap;
-`;
-const BodyLeft = styled.div``;
+`
+const BodyLeft = styled.div``
 
 const BodyRight = styled.div`
   display: flex;
   flex-flow: column;
   width: calc(100% - ${({ SidebarWidth }) => SidebarWidth});
   padding-bottom: 40px;
-`;
+`
 const CardContainer = styled.div`
   border: 1px solid #edf1f7;
   border-top: 0;
@@ -81,7 +100,7 @@ const CardContainer = styled.div`
   height: 100%;
   padding-inline: 24px;
   padding-block: 40px;
-`;
+`
 const CardWrapper = styled.div`
   width: 100%;
   display: grid;
@@ -91,4 +110,12 @@ const CardWrapper = styled.div`
     grid-template-columns: auto auto auto;
     gap: 24px;
   }
+`
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 40px;
+  height: ${({ height }) => height && height};
 `;
