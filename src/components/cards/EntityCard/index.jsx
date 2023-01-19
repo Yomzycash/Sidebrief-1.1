386d57d79bf641ca622577f23d2cbf7e6f8064ff
jesuloba-world <jesulobajohn@gmail.com>
2023-01-19 10:46:15 +0100

    fixed pricing

--- src/components/cards/EntityCard/index.jsx
+++ src/components/cards/EntityCard/index.jsx
@@ -1,61 +1,61 @@
 import React from "react";
 import {
-  Container,
-  Corner,
-  Top,
-  Title,
-  TimeLine,
-  Bottom,
-  Mid,
-  Price,
-  Bullet,
-  Content,
+	Container,
+	Corner,
+	Top,
+	Title,
+	TimeLine,
+	Bottom,
+	Mid,
+	Price,
+	Bullet,
+	Content,
 } from "./styles";
 import { ReactComponent as CornerPetal } from "asset/svg/cornerPetal.svg";
 import { ReactComponent as Mark } from "asset/svg/mark.svg";
 import numeral from "numeral";
 
 export const EntityCard = ({
-  action,
-  name,
-  price,
-  timeline,
-  requirement,
-  shares,
-  type,
-  currency,
+	action,
+	name,
+	price,
+	timeline,
+	requirement,
+	shares,
+	type,
+	currency,
 }) => {
-  return (
-    <Container onClick={action}>
-      <Corner>
-        <CornerPetal />
-      </Corner>
-      <Top>
-        <Title>{name}</Title>
-        <TimeLine>{timeline}</TimeLine>
-      </Top>
-      <Mid>
-        <Price>
-          {price} {currency}
-        </Price>
-      </Mid>
+	return (
+		<Container onClick={action}>
+			<Corner>
+				<CornerPetal />
+			</Corner>
+			<Top>
+				<Title>{name}</Title>
+				<TimeLine>{timeline}</TimeLine>
+			</Top>
+			<Mid>
+				<Price>
+					{numeral(price).format("0,0")} {currency}
+				</Price>
+			</Mid>
 
-      <Bottom>
-        {/* <Bullet>
+			<Bottom>
+				{/* <Bullet>
           <Mark /> <Content>{company}</Content>
         </Bullet> */}
-        <Bullet>
-          <Mark /> <Content>{type} Company</Content>
-        </Bullet>
-        <Bullet>
-          <Mark /> <Content>{shares} shares</Content>
-        </Bullet>
-        <Bullet>
-          <Mark /> <Content>{requirement}</Content>
-        </Bullet>
-      </Bottom>
-    </Container>
-  );
+				<Bullet>
+					<Mark /> <Content>{type} Company</Content>
+				</Bullet>
+				<Bullet>
+					<Mark /> <Content>{shares} shares</Content>
+				</Bullet>
+				<Bullet>
+					<Mark /> <Content>{requirement}</Content>
+				</Bullet>
+			</Bottom>
+		</Container>
+	);
 };
 
 export { Wrapper as EntityWrapper } from "./wrapper";
