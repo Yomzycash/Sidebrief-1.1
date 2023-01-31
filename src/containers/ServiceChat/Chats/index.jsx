9f8fa12aaa07150482ff25fbe5057a19918e42b6
femiAdeyemo <femi@sidebrief.com>
2023-01-31 16:12:16 +0100

    chat side

--- src/containers/ServiceChat/Chats/index.jsx
+++ src/containers/ServiceChat/Chats/index.jsx
@@ -5,26 +5,59 @@ import {
   DropDown,
   SearchContainer,
   TextContainer,
-  Option,
+  ArrowDown,
   TopContainer,
   ChatContainer,
+  DropDownBtn,
+  DropDownContent,
+  DropDownItems,
 } from './style'
 import numeral from 'numeral'
 import Search from 'components/navbar/Search'
 import { chatArray } from 'utils/config'
-import ChatCard from 'components/cards/ChatCard/ChatCard'
+import ChatCard from 'components/cards/ChatCard'
+import { IoIosArrowDown } from 'react-icons/io'
+import { useState } from 'react'
 
 export const Chats = () => {
+  const options = ['filter', 'name', 'chat']
+
+  const [selected, setSelected] = useState('')
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <Container>
       <TopContainer>
         <Head>
           <Heading>Chats ({numeral(2002).format('0,0')})</Heading>
-          <DropDown>
-            <TextContainer>
-              <Option value="filter">filter</Option>
-              <Option value="All">All</Option>
-            </TextContainer>
+         git  <DropDown>
+            <DropDownBtn
+              onClick={(e) => {
+                setIsActive(!isActive)
+              }}
+            >
+              <TextContainer> {selected}</TextContainer>
+              <ArrowDown
+                onClick={() => setIsActive(!isActive)}
+                isActive={isActive}
+              >
+                <IoIosArrowDown />
+              </ArrowDown>
+            </DropDownBtn>
+            {isActive && (
+              <DropDownContent>
+                {options.map((option) => (
+                  <DropDownItems
+                    onClick={(e) => {
+                      setSelected(option)
+                      setIsActive(false)
+                    }}
+                  >
+                    {option}
+                  </DropDownItems>
+                ))}
+              </DropDownContent>
+            )}
           </DropDown>
         </Head>
         <SearchContainer>
