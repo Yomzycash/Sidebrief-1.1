e33f8ba532c4e8efe20d9b7ac2c96c0a507e237c
jesuloba-world <jesulobajohn@gmail.com>
2023-02-05 19:25:48 +0100

    found a fix

--- src/containers/ServiceChat/Chats/actions.js
+++ src/containers/ServiceChat/Chats/actions.js
@@ -1,19 +1,24 @@
-import { formatDistanceToNow, subHours, parseJSON, compareAsc } from "date-fns";
+import { parseJSON, compareAsc } from "date-fns";
 
 export const getMessages = (data) => {
-  const uniqueSenders = [...new Set(data?.map((el) => el.senderId))];
+	const uniqueSenders = [...new Set(data?.map((el) => el.senderId))];
 
-  const uniqueData = uniqueSenders.map((el) => {
-    const relatedData = data?.filter(
-      (notification) => notification.senderID === el
-    );
-    return {
-      senderID: el,
-      notification: relatedData.sort((a, b) =>
-        compareAsc(parseJSON(a.createdAt), parseJSON(b.createdAt))
-      ),
-    };
-  });
+	const uniqueData = uniqueSenders
+		.map((el) => {
+			const relatedData = data?.filter(
+				(notification) => notification.senderID === el
+			);
 
-  return uniqueData;
+			return {
+				senderID: el,
+				notification: relatedData.sort((a, b) =>
+					compareAsc(parseJSON(a.createdAt), parseJSON(b.createdAt))
+				),
+			};
+		})
+		.filter((el) => {
+			return el.notification.length > 0;
+		});
+
+	return uniqueData;
 };
