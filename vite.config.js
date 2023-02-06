4ce3ec4b0cf4bd5ee6854273730a73877058938d
jesuloba-world <jesulobajohn@gmail.com>
2023-02-06 07:45:13 +0100

    lots of errors to fix

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteJsconfigPaths from "vite-jsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), viteJsconfigPaths(), svgrPlugin()],
+	build: {
+		outDir: "build",
+	},
+	server: {
+		open: true,
+		port: 3000,
+	},
+});
