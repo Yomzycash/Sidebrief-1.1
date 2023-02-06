7ae93a08f7acbf7bf907e9cf97ac7a7fc1862ead
jesuloba-world <jesulobajohn@gmail.com>
2023-02-06 07:58:12 +0100

    all errors fixed

--- src/pages/Dashboard/staffDashboard/Reward/Details/index.jsx
+++ src/pages/Dashboard/staffDashboard/Reward/Details/index.jsx
@@ -1,39 +1,38 @@
 import StaffRewardDetails from "components/staffRewardDetails";
 import React, { useEffect, useState } from "react";
-import { setStaffReward, staffRewardInfo } from "redux/Slices";
 import { store } from "redux/Store";
 import { useGetAllRewardsQuery } from "services/RewardService";
 import { Body, Container, Loading } from "./style";
 import { Puff } from "react-loading-icons";
 
 const StaffRewardDetailsPage = () => {
-  const [selectedReward, setSelectedReward] = useState([]);
-  const { data, isLoading, isError, isSuccess } = useGetAllRewardsQuery({
-    refetchOnMountOrArgChange: true,
-  });
+	const [selectedReward, setSelectedReward] = useState([]);
+	const { data, isLoading, isError, isSuccess } = useGetAllRewardsQuery({
+		refetchOnMountOrArgChange: true,
+	});
 
-  useEffect(() => {
-    let localRewardID = localStorage.getItem("rewardId");
-    let rewardID = JSON.parse(localRewardID);
-    const rewardData = data === undefined ? [] : [...data];
-    const rewardDatails = rewardData.filter(
-      (data) => data.rewardID === rewardID
-    );
-    setSelectedReward(rewardDatails);
-  }, [data]);
-  return (
-    <Container>
-      <Body>
-        {isLoading ? (
-          <Loading height="300px">
-            <Puff stroke="#00A2D4" fill="white" width={60} />
-          </Loading>
-        ) : (
-          <StaffRewardDetails selectedReward={selectedReward} />
-        )}
-      </Body>
-    </Container>
-  );
+	useEffect(() => {
+		let localRewardID = localStorage.getItem("rewardId");
+		let rewardID = JSON.parse(localRewardID);
+		const rewardData = data === undefined ? [] : [...data];
+		const rewardDatails = rewardData.filter(
+			(data) => data.rewardID === rewardID
+		);
+		setSelectedReward(rewardDatails);
+	}, [data]);
+	return (
+		<Container>
+			<Body>
+				{isLoading ? (
+					<Loading height="300px">
+						<Puff stroke="#00A2D4" fill="white" width={60} />
+					</Loading>
+				) : (
+					<StaffRewardDetails selectedReward={selectedReward} />
+				)}
+			</Body>
+		</Container>
+	);
 };
 
 export default StaffRewardDetailsPage;
