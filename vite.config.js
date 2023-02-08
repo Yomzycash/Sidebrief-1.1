9536c4ae3c06c1986022ee49fc84c4bfb14afa4d
jesuloba-world <jesulobajohn@gmail.com>
2023-02-08 14:28:00 +0100

    refix: merge conflicts solved

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import viteJsconfigPaths from "vite-jsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import eslint from "vite-plugin-eslint";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteJsconfigPaths(), svgrPlugin(), eslint()],
+	plugins: [react(), viteJsconfigPaths(), svgrPlugin(), eslint(), legacy()],
 	build: {
 		outDir: "build",
 	},
