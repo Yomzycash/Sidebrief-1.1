effc4e8fa7697eebf271fbc59ee31e65163fb801
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-02-08 15:28:44 +0100

    removed legacy

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteJsconfigPaths from "vite-jsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteJsconfigPaths(), svgrPlugin(), legacy()],
+	plugins: [react(), viteJsconfigPaths(), svgrPlugin()],
 	build: {
 		outDir: "build",
 	},
