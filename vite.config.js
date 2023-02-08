628e89a83920f64baf1f1a368d53c64d0ef346da
jesuloba-world <jesulobajohn@gmail.com>
2023-02-08 13:31:55 +0100

    removed unused variable

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteJsconfigPaths from "vite-jsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-import path from "path";
 import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
