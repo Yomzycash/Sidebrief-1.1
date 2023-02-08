fef39de5f9b09297cff6244fbf7b5e31336ce2a8
jesuloba-world <jesulobajohn@gmail.com>
2023-02-08 13:30:16 +0100

    legacy support added

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import viteJsconfigPaths from "vite-jsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import path from "path";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteJsconfigPaths(), svgrPlugin()],
+	plugins: [react(), viteJsconfigPaths(), svgrPlugin(), legacy()],
 	build: {
 		outDir: "build",
 	},
