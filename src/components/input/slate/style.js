aec760328be53c81045d79822256554cb7dd69f2
jesuloba-world <jesulobajohn@gmail.com>
2023-02-20 12:20:27 +0100

    slate now submits

--- src/components/input/slate/style.js
+++ src/components/input/slate/style.js
@@ -1,87 +1,90 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-  flex: 1;
-  width: 100%;
+	flex: 1;
+	width: 100%;
 
-  height: 150px;
-  padding: 0 13px 16px;
-  flex: 1;
-  /* border: none; */
+	height: 150px;
+	padding: 0 13px 16px;
+	flex: 1;
+	/* border: none; */
 
-  font-family: 'BR Firma';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 24px;
-  color: #000;
-  resize: none;
-  /* position: relative; */
-  overflow-y: auto;
+	font-family: "BR Firma";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 24px;
+	color: #000;
+	resize: none;
+	/* position: relative; */
+	overflow-y: auto;
 
-  &::placeholder {
-    color: #727474;
-  }
+	&::placeholder {
+		color: #727474;
+	}
 
-  blockquote {
-    margin-left: 0px;
-    margin-right: 0px;
-    padding-left: 10px;
-    color: rgb(170, 170, 170);
-    font-style: italic;
-    border-left: 2px solid rgb(221, 221, 221);
-  }
+	blockquote {
+		margin-left: 0px;
+		margin-right: 0px;
+		padding-left: 10px;
+		color: rgb(170, 170, 170);
+		font-style: italic;
+		border-left: 2px solid rgb(221, 221, 221);
+	}
 
-  .editor {
-    & > * + * {
-      margin-top: 1em;
-    }
-  }
+	.editor {
+		height: fit-content;
+		width: 100%;
 
-  ol {
-    list-style-type: decimal;
-  }
+		& > * + * {
+			margin-top: 1em;
+		}
+	}
 
-  ul {
-    list-style-type: disc;
-  }
+	ol {
+		list-style-type: decimal;
+	}
 
-  ol,
-  ul {
-    display: block;
-    margin-block-start: 1em;
-    margin-block-end: 1em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    padding-inline-start: 40px;
-  }
-`
+	ul {
+		list-style-type: disc;
+	}
+
+	ol,
+	ul {
+		display: block;
+		margin-block-start: 1em;
+		margin-block-end: 1em;
+		margin-inline-start: 0px;
+		margin-inline-end: 0px;
+		padding-inline-start: 40px;
+	}
+`;
 
 export const ButtonSpan = styled.span`
-  cursor: pointer;
-  color: ${({ reversed, active }) =>
-    reversed ? (active ? 'white' : '#aaa') : active ? 'black' : '#ccc'};
-`
+	cursor: pointer;
+	color: ${({ reversed, active }) =>
+		reversed ? (active ? "white" : "#aaa") : active ? "black" : "#ccc"};
+`;
 
 export const IconSpan = styled.span`
-  font-size: 18px;
-  vertical-align: text-bottom;
-`
+	font-size: 18px;
+	vertical-align: text-bottom;
+`;
 
 export const MenuDiv = styled.div`
-  position: sticky;
-  top: 0;
-  padding: 15px 18px 5px;
-  margin: 0 -13px;
-  border-bottom: 2px solid #eee;
-  margin-bottom: 10px;
-  background: #fff;
-  z-index: 2;
+	position: sticky;
+	top: 0;
+	padding: 15px 18px 5px;
+	margin: 0 -13px;
+	border-bottom: 2px solid #eee;
+	margin-bottom: 10px;
+	background: #fff;
+	z-index: 2;
 
-  & > * {
-    display: inline-block;
-  }
-  & > * + * {
-    margin-left: 15px;
-  }
-`
+	& > * {
+		display: inline-block;
+	}
+	& > * + * {
+		margin-left: 15px;
+	}
+`;
--- src/containers/ServiceChat/ChatBody/chatInput/index.jsx
+++ src/containers/ServiceChat/ChatBody/chatInput/index.jsx
@@ -1,83 +1,46 @@
-import { SubjectInput, TextBody, TextInput, TextInputForm } from './style'
-import { CommonButton } from 'components/button'
-import { Send } from 'asset/svg'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { messageSchema } from '../constants'
-import { SlateEditor } from 'components/input'
-import { useAddNotificationMutation } from 'services/chatService'
-import { useLocation } from 'react-router-dom'
-import { useState } from 'react'
+import { SubjectInput, TextBody, TextInput, TextInputForm } from "./style";
+import { CommonButton } from "components/button";
+import { Send } from "asset/svg";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { messageSchema } from "../constants";
+import { SlateEditor } from "components/input";
 
 export const ChatInput = ({ noEditor }) => {
-  const { handleSubmit, register, reset } = useForm({
-    resolver: yupResolver(messageSchema),
-  })
-  const [subjectText, setSubjectText] = useState('')
-  const [bodyText, setBodyText] = useState('')
+	const {
+		handleSubmit,
+		register,
+		reset,
+		setValue,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(messageSchema),
+	});
 
-  const [addNotification] = useAddNotificationMutation()
-  const location = useLocation()
-  let params = new URLSearchParams(location.search)
-  let serviceId = params.get('serviceId')
-  const username = JSON.parse(localStorage.getItem('userInfo'))
+	const sendMessage = (data) => {
+		console.log(data);
+		reset();
+	};
 
-  const SubmitForm = async (data) => {
-    const requiredChatData = {
-      serviceId: serviceId,
-      senderId: username?.username,
-      messageSubject: 'Re: Please upload signature',
-      messageBody: "Gbubemi's message ",
-      messageIsRead: false,
-      messageFiles: [
-        {
-          fileUrl: 'www.link.com',
-          fileName: 'passportlocal',
-          fileType: 'pdf',
-        },
-      ],
-    }
-    reset()
-  }
-  const handleSubjectChange = (e) => {
-    setSubjectText(e.target.value)
-  }
-  const handleBodyChange = (e) => {
-    setBodyText(e.target.value)
-    console.log(e)
-  }
-
-  const sendMessage = (data) => {
-    // console.log('femi', serviceId)
-    reset()
-  }
-
-  
-
-  return (
-    <TextInputForm onSubmit={handleSubmit(sendMessage)}>
-      <SubjectInput
-        placeholder="Subject"
-        {...register('subject')}
-        onChange={handleSubjectChange}
-        value={subjectText}
-      />
-      <TextBody>
-        {/* <TextInput
-					placeholder="Send a message"
-					{...register("message")}
-				/> */}
-        {!noEditor && (
-          <>
-            <SlateEditor
-              bodyText={bodyText}
-              placeholder="Send a message..."
-              onChange={handleBodyChange}
-            />
-          </>
-        )}
-        <CommonButton text={'Send'} RightIcon={Send} />
-      </TextBody>
-    </TextInputForm>
-  )
-}
+	return (
+		<TextInputForm onSubmit={handleSubmit(sendMessage)}>
+			<SubjectInput placeholder="Subject" {...register("subject")} />
+			<TextBody>
+				{!noEditor ? (
+					<>
+						<SlateEditor
+							placeholder="Send a message..."
+							setValue={setValue}
+						/>
+					</>
+				) : (
+					<TextInput
+						placeholder="Send a message"
+						{...register("message")}
+					/>
+				)}
+				<CommonButton type={"submit"} text={"Send"} RightIcon={Send} />
+			</TextBody>
+		</TextInputForm>
+	);
+};
