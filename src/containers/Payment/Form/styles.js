76ca16c8b04b0b7bfc1083703c01aee394b2f600
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-02-22 22:47:31 +0100

    stripe

--- src/containers/Payment/Form/styles.js
+++ src/containers/Payment/Form/styles.js
@@ -158,4 +158,13 @@ export const Paystack = styled.div`
 
 export const ButtonContainer = styled.form`
   width: 80%;
+  display: flex;
+  flex-flow: column;
+  gap: 24px;
+`;
+export const FormData = styled.div`
+  border: solid red;
+  padding: 12px 24px;
+  border: 1.5px solid #edf1f7;
+  border-radius: 8px;
 `;
