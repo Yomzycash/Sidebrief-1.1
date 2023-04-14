4c14eb7abfd2d7cfbb0b3d99d8274196cbc4c419
Damola Olutoke <olutokeezekiel18@gmail.com>
2023-04-14 22:38:56 +0100

    Crud operations on banks successful

---
+++ src/components/staffBankDetails/style.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  background: #ffffff;
+  border: 1px solid #edf1f7;
+  box-shadow: 0px 10px 10px -5px #9596970a;
+  border-radius: 16px;
+  gap: 24px;
+  display: flex;
+  flex-flow: column;
+  padding: 40px 24px;
+  width: 100%;
+`;
+
+export const FullContentWrapper = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: 1fr;
+	row-gap: 24px;
+	column-gap: 24px;
+	padding-bottom:10px;
+
+	@media screen and (max-width: 900px) {
+		grid-template-columns: 1fr;
+	}
+`;
+
+export const ContentWrapper = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(2, minmax(50px, 1fr));
+	row-gap: 24px;
+	column-gap: 24px;
+	padding-bottom:10px;
+
+	@media screen and (max-width: 900px) {
+		display: flex;
+		flex-flow: column;
+	}
+`;
+export const TextWithLabel = styled.div`
+	width: 100%;
+`;
+
+export const Label = styled.p`
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 21px;
+	margin-bottom: 10px;
+	color: #4e5152;
+`;
+
+export const TextWrapper = styled.div`
+	padding: 16px 24px;
+	gap: 16px;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	background: #fafafa;
+	border: 1px solid #edf1f7;
+	border-radius: 8px;
+	max-width: 100%;
+`;
+export const Text = styled.div`
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 21px;
+	/* identical to box height, or 150% */
+
+	display: flex;
+	align-items: center;
+
+	/* Grey 1 */
+
+	color: ${({ theme }) => theme.grey1};
+`;
+export const LinkText = styled.p`
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 21px;
+	text-decoration-line: underline;
+	color: #00a2d4;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	overflow: hidden;
+	cursor: pointer;
+	max-width: 100%;
+`;
---
+++ src/pages/Dashboard/staffDashboard/Banks/AllBanks/index.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react'
+import StaffBankHeader from "components/Header/StaffBankHeader"
+import {
+    // Body,
+    // BodyLeft,
+    BodyRight,
+    // Container,
+    ListItem,
+    ListItems,
+    ListItemsContainer,
+    BankContainer,
+    Loading,
+  } from "./style";
+import { useSelector } from "react-redux";
+import { Puff } from "react-loading-icons";
+import PetalsCard from "components/cards/RewardCard/PetalsCard";
+import StaffBankModal from "components/modal/StaffBankModal";
+import { RewardCard } from "components/cards";
+import { 
+    useGetAllBanksQuery,
+    useAddBankMutation,
+
+ } from "services/staffService";
+ import { useNavigate, useSearchParams } from "react-router-dom";
+import { toast } from 'react-hot-toast';
+import { handleError } from "utils/globalFunctions";
+
+
+const Banks = () => {
+    const [open, setOpen] = useState(false);
+    const [filteredBanks, setFilteredBanks] = useState([]);
+    const layouInfo = useSelector((store) => store.LayoutInfo);
+    const sidebarWidth = {layouInfo};
+    const navigate = useNavigate();
+    const { data, isLoading, refetch } = useGetAllBanksQuery();
+    const [addBank, addState] = useAddBankMutation();
+
+
+
+    useEffect(() => {
+        setFilteredBanks(data);
+    }, [data])
+
+    const handleBankClick = (bankCode) => {
+        navigate("/staff-dashboard/bank-accounts/bank/details");
+        localStorage.setItem("bankCode", JSON.stringify(bankCode));
+    }
+
+    // Returns the data to be sent to the backend
+    const getRequired = (formData) => {
+        return {
+            bankName:formData.bank_name,
+            bankDescription:formData.description,
+            bankCode:formData.bank_code,
+            bankCountry: formData.country,
+            bankUrl:formData.bank_url,
+            bankLogo:formData.image
+        }
+    }
+
+    // This functtion is used to add a reward. It runs when the form gets submitted.
+    const submitAction = async (formData) => {
+        let requiredData = getRequired(formData)
+        let response = await addBank(requiredData);
+        let data = response?.data;
+        let error = response?.error;
+
+        if(data){
+            toast.success("Bank added succcessfully");
+            setOpen(false);
+        } else {
+            handleError(error)
+        }
+        refetch();
+        console.log("formaDta for banks", formData)
+    }
+    return (
+        <BodyRight sidebarWidth={sidebarWidth}>
+            <StaffBankHeader setOpen={setOpen} />
+            { isLoading ? (
+                <Loading height="300px">
+                    <Puff stroke="#00A2D4" fill="white" width={60} />
+              </Loading>
+            ) : (
+               <BankContainer>
+                    {filteredBanks?.map((bank, index) => (
+                        <RewardCard
+                            key={index}
+                            title={bank?.bankCountry}
+                            body={bank?.bankName}
+                            image={bank?.bankLogo}
+                            submitAction={submitAction}
+                            action={() => handleBankClick(bank.bankCode)}
+                            rewardspage
+                        />
+                    ))}
+               </BankContainer>
+            )}
+            <StaffBankModal
+                setOpen={setOpen}
+                open={open}
+                submitAction={submitAction}
+                loading={addState.isLoading}
+            />
+        </BodyRight>
+    )
+}
+
+export default Banks
