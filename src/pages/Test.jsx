aee735fa22f1faff83fea26835c81fc44695f006
Damola Olutoke <olutokeezekiel18@gmail.com>
2023-04-14 22:49:00 +0100

    fixes

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import UserService from "./Dashboard/User/Service";
+import StaffRewardDetailsPage from "./Dashboard/staffDashboard/Reward/Details";
+import StaffBankDetailsPage from "./Dashboard/staffDashboard/Banks/Details";
+import Banks from "./Dashboard/staffDashboard/Banks";
 
 const Test = () => {
   return (
     <div style={{}}>
-      <UserService />
+      <StaffBankDetailsPage />
+      {/* <Banks/> */}
     </div>
   );
 };
 
 export default Test;
+
